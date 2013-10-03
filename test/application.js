e4fac5befe278dca3453b20fc3c4440cf13700a3
Dennis Heaney <dennis@learningpool.com>
2013-10-03 22:11:57 +0200

    Task #8103: Create module and define methods
    Change-Id: Ia16ce72ce229d03236110a18321ae18720980423

--- test/application.js
+++
@@ -1,9 +0,0 @@
-var builder = require('../');
-
-describe('application', function(){
-    it ('should inherit from event emmiter', function(done){
-        var app = builder();
-        app.on('foo', done);
-        app.emit('foo');
-    });
-});
---
+++ test/lib.configuration.js
@@ -0,0 +1,58 @@
+var fs = require('fs'),
+    path = require('path'),
+    configuration = require('../lib/configuration');
+
+describe('configuration', function() {
+  var tmpDir = path.join(configuration.serverRoot, 'tmp');
+  var tmpFile = path.join(tmpDir, 'tempConfig.json');
+  before(function(){
+    // create a temp config file
+    if (!fs.existsSync(tmpDir)) {
+      fs.mkdirSync(tmpDir);
+    }
+
+    if (!fs.existsSync(tmpFile)) {
+      var fd = fs.openSync(tmpFile, 'w');
+      fs.writeSync(fd, JSON.stringify({
+          serverPort: '4321'
+      }));
+    }
+  });
+
+  after(function(){
+    // delete the temp config file
+    if (fs.existsSync(tmpFile)) {
+      fs.unlinkSync(tmpFile);
+    }
+
+    if (fs.existsSync(tmpDir)) {
+      fs.rmdirSync(tmpDir);
+    }
+  });
+
+  it('should allow me to load a configuration from disk', function() {
+    configuration.load(tmpFile);
+    configuration.getConfig('serverPort').should.equal('4321');
+  });
+
+  it('should allow me to set/get a global configuration variable', function(){
+    configuration.setConfig('foo', 10);
+    configuration.getConfig('foo').should.equal(10);
+  });
+  
+  it('should allow me to set/get a module configuration variable', function(){
+    configuration.setModuleConfig('foo', 'bar', 10);
+    configuration.getModuleConfig('foo', 'bar').should.equal(10);
+  });
+
+  it('should allow me to retrieve all config values for a module', function(){
+    // add a few configs for module foo
+    configuration.setModuleConfig('foo', 'bar', 10);
+    configuration.setModuleConfig('foo', 'fizz', 'buzz');
+
+    // retrieve the config object for foo and verify
+    var moduleConfig = configuration.getModuleConfig('foo');
+    moduleConfig.should.have.property('bar', 10);
+    moduleConfig.should.have.property('fizz', 'buzz');
+  });
+});

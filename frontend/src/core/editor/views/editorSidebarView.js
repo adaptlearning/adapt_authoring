716b092d3d74ab6e3572ddfa876353f8cc5f0638
Daryl Hedley <darylhedley@hotmail.com>
2014-02-28 10:17:21 +0000

    Updated standard classes and editor sidebar

--- frontend/src/core/editor/views/editorSidebarView.js
+++ frontend/src/core/editor/views/editorSidebarView.js
@@ -11,8 +11,19 @@ define(function(require) {
 			this.listenTo(AdaptBuilder, 'editorSidebar:addEditView', this.addEditingView);
 		},
 
-		addEditingView: function() {
+		addEditingView: function(view) {
+			this.hideLoadingStatus();
+			this.hideInstruction();
+			this.$('.editor-sidebar-inner').append(view);
 			console.log('adding editing view');
+		},
+
+		hideLoadingStatus: function() {
+			this.$('.editor-sidebar-loading').addClass('display-none');
+		},
+
+		hideInstruction: function() {
+			this.$('.editor-sidebar-instruction').addClass('display-none');
 		}
 
 	}, {

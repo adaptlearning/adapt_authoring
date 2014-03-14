be67fd16d9d6f05aba57bd180dcce7250d6ba1f9
brian-learningpool <brian@learningpool.com>
2014-03-14 11:48:06 +0000

    Minor re-work to router
    - Organised routes for readability
    - Added re-direct to Login when user is not authenticated

--- frontend/src/core/app/helpers.js
+++ frontend/src/core/app/helpers.js
@@ -2,51 +2,51 @@ define(function(require){
 
     var Handlebars = require('handlebars');
     var helpers = {
-            lowerCase: function(text) {
-                return text.toLowerCase();
-            },
-            numbers: function(index) {
-                return index +1;
-            },
-            capitalise:  function(text) {
-                return text.charAt(0).toUpperCase() + text.slice(1);
-            },
-            odd: function (index) {
-                return (index +1) % 2 === 0  ? 'even' : 'odd';
-            },
-            formatDate: function(isoDate) {
-                // 2014-02-17T17:00:34.196Z
-                var date = new Date(isoDate);
+        lowerCase: function(text) {
+            return text.toLowerCase();
+        },
+        numbers: function(index) {
+            return index +1;
+        },
+        capitalise:  function(text) {
+            return text.charAt(0).toUpperCase() + text.slice(1);
+        },
+        odd: function (index) {
+            return (index +1) % 2 === 0  ? 'even' : 'odd';
+        },
+        formatDate: function(isoDate) {
+            // 2014-02-17T17:00:34.196Z
+            var date = new Date(isoDate);
 
-                return date.toDateString();
-            },
-            if_value_equals: function(value, text, block) {
-                if (value === text) {
-                    return block.fn(this);
-                } else {
-                    return block.inverse();
-                }
-            },
-            selected: function(option, value){
-                if (option === value) {
-                    return ' selected';
-                } else {
-                    return ''
-                }
-            },
-            counterFromZero: function(n, block) {
-                var sum = '';
-                for (var i = 0; i <= n; ++i)
-                    sum += block.fn(i);
-                return sum;
-            },
-            counterFromOne: function(n, block) {
-                var sum = '';
-                for (var i = 1; i <= n; ++i)
-                    sum += block.fn(i);
-                return sum;
+            return date.toDateString();
+        },
+        if_value_equals: function(value, text, block) {
+            if (value === text) {
+                return block.fn(this);
+            } else {
+                return block.inverse();
             }
-        };
+        },
+        selected: function(option, value){
+            if (option === value) {
+                return ' selected';
+            } else {
+                return ''
+            }
+        },
+        counterFromZero: function(n, block) {
+            var sum = '';
+            for (var i = 0; i <= n; ++i)
+                sum += block.fn(i);
+            return sum;
+        },
+        counterFromOne: function(n, block) {
+            var sum = '';
+            for (var i = 1; i <= n; ++i)
+                sum += block.fn(i);
+            return sum;
+        }
+    };
 
     for(var name in helpers) {
        if(helpers.hasOwnProperty(name)) {

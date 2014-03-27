cf4e6cf9e4e74e0478a0820423960a1e7558bd33
Kevin Corry <kevinc@learningpool.com>
2014-03-27 16:22:39 +0000

    Password reset feature - Generates a token which can be used to reset the users password, which will expire after a length of time - Outstanding - mailer, clean-up task for stale tokens

--- frontend/src/core/user/views/forgotPasswordView.js
+++ frontend/src/core/user/views/forgotPasswordView.js
@@ -6,7 +6,25 @@ define(function(require) {
     
     tagName: "div",
 
-    className: "forgotpassword"
+    className: "forgot-password",
+
+    events: {
+      'click .form-forgot-password .submit':'requestResetToken'
+    },
+
+    requestResetToken: function(e) {
+      e.preventDefault();
+
+      var email = this.$('.input-username-email').val();
+      var model = this.model;
+      var view = this;
+
+      model.generateResetToken(email, function(err, result) {
+        // Display success regardless - we don't want to give them any clues :)
+        view.$('.form-forgot-password').addClass('display-none');
+        view.$('.success').removeClass('display-none');
+      });
+    }
 
   }, {
     template: 'forgotPassword'

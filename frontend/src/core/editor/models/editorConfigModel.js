92528a7899af3dc65e10019a6ab2ab62ac5b77d6
Kevin Corry <kevinc@learningpool.com>
2014-04-09 09:09:14 +0100

    Fixes for content plugin

--- frontend/src/core/editor/models/editorConfigModel.js
+++ frontend/src/core/editor/models/editorConfigModel.js
@@ -6,23 +6,7 @@ define(function(require) {
   var EditorConfigModel = EditorModel.extend({
     urlRoot: '/api/content/config',
     
-    initialize : function(options) {
-      this.on('sync', this.loadedData, this);
-
-      if (options && options.courseId) {
-        this.url = '/api/content/config?_courseId=' + options.courseId;
-      }
-      this.fetch();
-    },
-
-    loadedData: function() {
-      if (this.constructor._siblings) {
-        this._type = this.constructor._siblings;
-      } else {
-        this._type = 'config';
-      }
-      Origin.trigger('editorModel:dataLoaded', this._type);
-    },
+    initialize : function() {}
   },
   {
     _parent: 'course',
--- plugins/content/config/index.js
+++ plugins/content/config/index.js
@@ -2,28 +2,16 @@
  * Config content plugin
  */
 
-var contentmanager = require('../../../lib/contentmanager'),
-    ContentPlugin = contentmanager.ContentPlugin,
-    ContentTypeError = contentmanager.errors.ContentTypeError,
+var ContentPlugin = require('../../../lib/contentmanager').ContentPlugin,
     configuration = require('../../../lib/configuration'),
     database = require('../../../lib/database'),
     util = require('util'),
     path = require('path');
 
-// try and require our base
-var ContentObject = false;
-try {
-  ContentObject = require('../contentobject');
-} catch (error) {
-  // we only catch so we can log the error :)
-  logger.log('fatal', 'Config content plugin has an unmet dependency! (contentobject)');
-  throw new ContentTypeError('Config content plugin has an unmet dependency! (contentobject)');
-}
-
 function ConfigContent () {
 }
 
-util.inherits(ConfigContent, ContentObject);
+util.inherits(ConfigContent, ContentPlugin);
 
 /**
  * implements ContentObject#getModelName
@@ -32,15 +20,6 @@ util.inherits(ConfigContent, ContentObject);
  */
 ConfigContent.prototype.getModelName = function () {
   return 'config';
-};
-
-/**
- * returns the child type for this object
- *
- * @return string
- */
-ConfigContent.prototype.getChildType = function () {
-  return false; // no children yet!
 };
 
 /**

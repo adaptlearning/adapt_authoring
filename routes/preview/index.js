649f5cfdd0fa42d8dacc90ef8036b6ed388d38cc
Kevin Corry <kevinc@learningpool.com>
2014-06-24 10:35:02 +0100

    Adding the ability to preview a course

---
+++ routes/preview/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var server = module.exports = express();
+var AdaptOutputPlugin = require('../../plugins/output/adapt/index');
+var usermanager = require('../../lib/usermanager');
+
+server.set('views', __dirname);
+server.set('view engine', 'hbs');
+
+server.get('/preview/:course/:user/*', function (req, res, next) {
+  var index = 'main.html',
+      type = 'adapt',
+      course = req.params.course,
+      user = req.params.user,
+      file = req.params[0] || index,
+      serverRoot = path.normalize(path.join(__dirname, '../../')),
+      requestedFile = path.join(serverRoot, 'temp', course, user, 'build', file),
+      loggedInUser = usermanager.getCurrentUser();
+
+  if (user == loggedInUser._id) {
+    if (file == index) {
+      // Publish before preview
+      AdaptOutputPlugin.prototype.publish(course, true, req, res, function (error, result) {
+        if (error) {
+          res.json({ success: false, message: error.message });
+          return res.end();
+        }
+        res.sendfile(requestedFile);
+      });
+    } else {
+      // Just serve the file
+      res.sendfile(requestedFile);
+    }
+  } else {
+    // User doesn't have access to this course
+    res.statusCode = 500;
+    res.json({success: false});
+    return res.end();
+  }
+
+});

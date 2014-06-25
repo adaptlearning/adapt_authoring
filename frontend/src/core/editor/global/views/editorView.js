b67e84e8ff762194aef7425fd06f16940bb5a8d2
brian-learningpool <brian@learningpool.com>
2014-06-25 11:59:31 +0100

    Moved the preview/publish buttons to partial .hbs
    These buttons are now present and functional on editing the menu or page.

--- frontend/src/core/editor/global/views/editorView.js
+++ frontend/src/core/editor/global/views/editorView.js
@@ -49,8 +49,6 @@ define(function(require){
       this.listenTo(Origin, 'editorView:paste', this.pasteFromClipboard);
       this.listenTo(Origin, 'editorCommon:publish', this.publishProject);
       this.listenTo(Origin, 'editorCommon:preview', this.previewProject);
-      // this.listenTo(Origin, 'editorMenuSidebarView:publish', this.publishProject);
-      // this.listenTo(Origin, 'editorMenuSidebarView:preview', this.previewProject);
 
       this.render();
       this.setupEditor();

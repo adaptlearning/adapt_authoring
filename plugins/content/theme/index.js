747061f1431152b725d481842ac08b0aef0f3c91
Dennis Heaney <dennis@learningpool.com>
2014-08-05 10:55:16 +0100

    issue #456 - updated config schema

--- plugins/content/theme/index.js
+++ plugins/content/theme/index.js
@@ -96,10 +96,22 @@ function initialize () {
         return res.json({ success: false, message: 'theme param should be an object id' });
       }
 
-      // @TODO - implement theme disable
-      res.statusCode = 200;
-      res.json({success: true});
-      return res.end();
+      // remove current theme from course
+      database.getDatabase(function (err, db) {
+        if (err) {
+          return next(err);
+        }
+
+        // update the course config object
+        db.update('config', { _courseId: courseId }, { _theme: null }, function (err) {
+          if (err) {
+            return next(err);
+          }
+          res.statusCode = 200;
+          res.json({success: true});
+          return res.end();
+        });
+      });
     });
 
     // enable a theme
@@ -113,9 +125,22 @@ function initialize () {
         return res.json({ success: false, message: 'theme param should be a object id' });
       }
 
-      res.statusCode = 200;
-      res.json({success: true});
-      return res.end();
+      // add selected theme to course config
+      database.getDatabase(function (err, db) {
+        if (err) {
+          return next(err);
+        }
+
+        // update the course config object
+        db.update('config', { _courseId: courseId }, { _theme: theme }, function (err) {
+          if (err) {
+            return next(err);
+          }
+          res.statusCode = 200;
+          res.json({success: true});
+          return res.end();
+        });
+      });
     });
   });
 }

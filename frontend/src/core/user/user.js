8ee16f6f5e33fe687f51f508700b3ff2502b7231
brian-learningpool <brian@learningpool.com>
2014-10-09 17:35:21 +0100

    On-going tidy up of user profile
    Minor changes:
    - Removed unused/old files
    - Fixed log out call by removing confirm screen

--- frontend/src/core/user/user.js
+++ frontend/src/core/user/user.js
@@ -1,75 +1,64 @@
 define(function(require) {
 
-  var Origin = require('coreJS/app/origin');
-  var LoginView = require('coreJS/user/views/loginView');
-  var UserProfileView = require('coreJS/user/views/userProfileView');
-  var UserProfileSidebarView = require('coreJS/user/views/userProfileSidebarView');
-  var UserProfileModel = require('coreJS/user/models/userProfileModel');
+	var Origin = require('coreJS/app/origin');
+	var LoginView = require('coreJS/user/views/loginView');
+	var UserProfileView = require('coreJS/user/views/userProfileView');
+	var UserProfileSidebarView = require('coreJS/user/views/userProfileSidebarView');
+	var UserProfileModel = require('coreJS/user/models/userProfileModel');
 
-  var EditView = require('coreJS/user/views/editView');
-  var LogoutView = require('coreJS/user/views/logoutView');
-  var ForgotPasswordView = require('coreJS/user/views/forgotPasswordView');
-  var ResetPasswordView = require('coreJS/user/views/resetPasswordView');
-  var UserPasswordResetModel = require('coreJS/user/models/userPasswordResetModel');
-  
+	var ForgotPasswordView = require('coreJS/user/views/forgotPasswordView');
+	var ResetPasswordView = require('coreJS/user/views/resetPasswordView');
+	var UserPasswordResetModel = require('coreJS/user/models/userPasswordResetModel');
 
-  Origin.on('navigation:user:logout', function() {
-    Origin.router.navigate('#/user/logout');
-  });
+	Origin.on('navigation:user:logout', function() {
+		Origin.router.navigate('#/user/logout');
+	});
 
-// Listen to the click on the email
-  Origin.on('navigation:profile:toggle', function() {
-    Origin.router.navigate('#/user/profile');
-  });
+	Origin.on('navigation:user:profile', function() {
+		Origin.router.navigate('#/user/profile');
+	});
 
+	Origin.on('router:user', function(location, subLocation, action) {
+		var currentView;
+		var settings = {};
 
-  Origin.on('router:user', function(location, subLocation, action) {
-    console.log(location, subLocation);
+		settings.authenticate = false;
 
-    var currentView;
-    var settings = {};
-    settings.authenticate = false;
+		switch (location) {
+			case 'login':
+			Origin.trigger('location:title:hide');
+			currentView = LoginView;
+			break;
+			case 'logout':
+			Origin.sessionModel.logout();
+			break;
+			case 'forgot':
+			currentView = ForgotPasswordView;
+			break;
+			case 'reset':
+			currentView = ResetView;
+			break;      
+			case 'profile':
+			settings.authenticate = true;
 
-    switch (location) {
-      case 'login':
-        Origin.trigger('location:title:hide');
-        currentView = LoginView;
-        break;
-      case 'logout':
-        Origin.trigger('location:title:hide');
-        currentView = LogoutView;
-        break;
-      case 'forgot':
-        currentView = ForgotPasswordView;
-        break;
-      case 'reset':
-        currentView = ResetView;
-        break;      
-      case 'profile':
-        settings.authenticate = true;
+			Origin.trigger('location:title:update', {title: window.polyglot.t('app.editprofileinformation')});        
+			currentView = UserProfileView;
+			break;
+		}
 
-        Origin.trigger('location:title:update', {title: window.polyglot.t('app.editprofileinformation')});        
-        currentView = UserProfileView;
-        break;
-    }
-
-    if (currentView) {      
-      if(location == 'profile'){
-        var profile = new UserProfileModel(); 
-        profile.fetch({
-          success: function() {
-            Origin.sidebar.addView(new UserProfileSidebarView().$el);
-            Origin.router.createView(currentView, {model: profile}, settings);
-          }
-        });     
-
-      }else{
-        Origin.router.createView(currentView, {model: Origin.sessionModel}, settings);
-      }
-      
-    }
-    
-
-  });
+		if (currentView) {      
+			if (location == 'profile'){
+				var profile = new UserProfileModel(); 
+				profile.fetch({
+					success: function() {
+						Origin.sidebar.addView(new UserProfileSidebarView().$el);
+						Origin.router.createView(currentView, {model: profile}, settings);
+					}
+				});
+			} else {
+				Origin.router.createView(currentView, {model: Origin.sessionModel}, settings);
+			}
+		}
+	});
 
 })

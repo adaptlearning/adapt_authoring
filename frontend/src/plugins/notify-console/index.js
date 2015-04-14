0a943e973db06fe6ff62e83fe3b5f18fecb12b9d
Thomas Taylor <thomas.taylor@kineo.com>
2015-04-14 14:32:32 +0100

    Update notify-console plugin for compatibility with Notify changes
    Now only console.log 'log' notifications, rather than all

--- frontend/src/plugins/notify-console/index.js
+++ frontend/src/plugins/notify-console/index.js
@@ -1,28 +1,36 @@
 define(function(require){
 	var Notify = require('coreJS/notify/notify');
 
+	Notify.addTemplate('log', {
+		name: "",
+		_type: "log"
+	});
+
 	if(!console.error) console.error = console.log;
 	if(!console.warn) console.warn = console.log;
 	if(!console.info) console.info = console.log;
 	if(!console.debug) console.debug = console.log;
 
-	Notify.on('fatal', function(model) {
-		console.error(model.get('name') + ": " + model.get('body'));
-	});
-
-	Notify.on('error', function(model) {
-		console.error(model.get('name') + ": " + model.get('body'));
-	});
-
-	Notify.on('warn', function(model) {
-		console.warn(model.get('name') + ": " + model.get('body'));
-	});
-
-	Notify.on('info', function(model) {
-		console.info(model.get('name') + ": " + model.get('body'));
-	});
-
-	Notify.on('debug', function(model){
-		console.debug(model.get('name') + ": " + model.get('body'));
+	Notify.on('log', function(model) {
+		switch(model.get('_level')) {
+			case 'fatal':
+				console.error(model.toString());
+				break;
+			case 'error':
+				console.error(model.toString());
+				break;
+			case 'warn':
+				console.warn(model.toString());
+				break;
+			case 'info':
+				console.info(model.toString());
+				break;
+			case 'debug':
+				console.debug(model.toString());
+				break;
+			default:
+				console.log(model.toString());
+				break;
+		}
 	});
 });

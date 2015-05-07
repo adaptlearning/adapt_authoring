1d75c603eee85a27d8f4b6546e686bd140b8a49b
Thomas Taylor <thomas.taylor@kineo.com>
2015-05-07 16:57:11 +0100

    Initial changes for schema ordering

--- frontend/src/core/scaffold/scaffold.js
+++ frontend/src/core/scaffold/scaffold.js
@@ -1,6 +1,5 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-
 	var Origin = require('coreJS/app/origin');
 	var BackboneForms = require('backboneForms');
 	var BackboneFormsLists = require('backboneFormsLists');
@@ -15,22 +14,19 @@ define(function(require) {
 	var Schemas = require('coreJS/scaffold/schemas');
 	var Overrides = require('coreJS/scaffold/scaffoldOverrides');
 	var Helpers = require('coreJS/app/helpers');
+
 	var ActiveItemsModal = 0;
 	var isOverlayActive = false;
-
 	var Scaffold = {};
 	// Used to pass model data around the current form
 	var currentModel;
-
 	// Used to store already build schemas
 	var builtSchemas = {};
-
-	Backbone.Form.editors.List.Modal.ModalAdapter = ScaffoldItemsModalView;
-
 	var defaultValidators = _.keys(Backbone.Form.validators);
-	var types = ['string', 'number', 'boolean', 'object', 'objectid', 'date', 'array'];
 	var customValidators = [];
 	var customTemplates = [];
+
+	Backbone.Form.editors.List.Modal.ModalAdapter = ScaffoldItemsModalView;
 
 	Origin.on('scaffold:updateSchemas', function(callback, context) {
 		Origin.trigger('schemas:loadData', function() {
@@ -41,216 +37,276 @@ define(function(require) {
 
 	var resetSchemas = function() {
 		builtSchemas = {};
-	}
+	};
 
 	var setupSchemaFields = function(field, key, schema, scaffoldSchema) {
-
-		if (field.type === 'array') {
-
-			if (field.items && field.items.properties) {
-				scaffoldSchema[key] = {
-					type: 'List',
-					itemType: 'Object',
-					subSchema: field.items.properties,
-					fieldType: 'List'
-				}
-			} else {
-				if (field.inputType) {
-					var fieldObject = {
-						type: field.inputType,
-						help: field.help,
-						default: field.default,
-						fieldType: field.inputType
-					};
-
-					if (_.isObject(field.inputType)) {
-						fieldObject = _.extend(fieldObject, field.inputType);
-					}
-					scaffoldSchema[key] = fieldObject;
-				} else {
-					scaffoldSchema[key] = {
-						type: 'List',
-						itemType:field.items.inputType,
-						subSchema: field.items,
-						fieldType: field.items.inputType
-					}
-				}
-			}
-
-			var objectSchema = (schema[key].properties || schema[key].subSchema);
-			var scaffoldObjectSchema = scaffoldSchema[key].subSchema;
-
-			_.each(objectSchema, function(field, key) {
-				setupSchemaFields(field, key, objectSchema, scaffoldObjectSchema);
-			});
-			
-		} else if (field.type != 'object') {
-
-			var validators = [];
-			// Go through each validator checking whether this is a default
-			// or a custom validator
-			_.each(field.validators, function(validator) {
-				if (!_.isArray(validator) && _.isObject(validator)) {
-					// If validator is an object - must be a default
-					validators.push(validator);
-				} else {
-					// Check whether this is a defaultValidator
-					if (_.contains(defaultValidators, validator)) {
-						validators.push(validator);
-					} else {
-						// If custom - search custom validators
-						var customValidator = _.findWhere(customValidators, {name: validator});
-						if (!customValidator) {
-							return console.log('No validator of that sort - please register: "' + validator + '" by using Origin.scaffold.addCustomValidator(name, validatorMethod)');
-						}
-						// If match is found - add the method
-						validators.push(customValidator.validatorMethod);
-					}
-				}
-			})
-
-			// Check if inputType is an object
-			// if so extend
-
-			var fieldObject = {
-				type: field.inputType,
-				validators: validators,
-				help: field.help,
-				default: field.default,
-				fieldType: field.inputType
-			};
-
-			if (_.isObject(field.inputType)) {
-				fieldObject = _.extend(fieldObject, field.inputType);
-			}
-			scaffoldSchema[key] = fieldObject;
-
+		if('array' === field.type) {
+			setupArraySchemaField(field, key, schema, scaffoldSchema);
+		} else if('object' === field.type) {
+			setupObjectSchemaField(field, key, schema, scaffoldSchema);
 		} else {
-
-			scaffoldSchema[key] = {
-				type: 'Object',
-				subSchema: field.properties
-			}
-
-			var objectSchema = (schema[key].properties || schema[key].subSchema);
-			var scaffoldObjectSchema = scaffoldSchema[key].subSchema;
-
-			_.each(objectSchema, function(field, key) {
-				setupSchemaFields(field, key, objectSchema, scaffoldObjectSchema);
-			});
-
+			setupNonObjectSchemaField(field, key, schema, scaffoldSchema);
 		}
 
 		if (field.title) {
 			scaffoldSchema[key].title = field.title;
-			
+
 		} else if (field.type === 'object') {
 			scaffoldSchema[key].title = '';
 			scaffoldSchema[key].legend = field.legend;
 		}
-	}
+	};
+
+	var setupArraySchemaField = function(field, key, schema, scaffoldSchema) {
+		if (field.items && field.items.properties) {
+			scaffoldSchema[key] = {
+				type: 'List',
+				itemType: 'Object',
+				subSchema: field.items.properties,
+				fieldType: 'List'
+			}
+		} else {
+			if (field.inputType) {
+				var fieldObject = {
+					type: field.inputType,
+					help: field.help,
+					default: field.default,
+					fieldType: field.inputType
+				};
+
+				if (_.isObject(field.inputType)) {
+					fieldObject = _.extend(fieldObject, field.inputType);
+				}
+				scaffoldSchema[key] = fieldObject;
+			} else {
+				scaffoldSchema[key] = {
+					type: 'List',
+					itemType:field.items.inputType,
+					subSchema: field.items,
+					fieldType: field.items.inputType
+				}
+			}
+		}
+
+		var objectSchema = (schema[key].properties || schema[key].subSchema);
+		var scaffoldObjectSchema = scaffoldSchema[key].subSchema;
+
+		_.each(objectSchema, function(field, key) {
+			setupSchemaFields(field, key, objectSchema, scaffoldObjectSchema);
+		});
+	};
+
+	var setupNonObjectSchemaField = function(field, key, schema, scaffoldSchema) {
+		var validators = [];
+		// Go through each validator checking whether this is a default or a custom validator
+		_.each(field.validators, function(validator) {
+			if (!_.isArray(validator) && _.isObject(validator)) {
+				// If validator is an object - must be a default
+				validators.push(validator);
+			} else {
+				// Check whether this is a defaultValidator
+				if (_.contains(defaultValidators, validator)) {
+					validators.push(validator);
+				} else {
+					// If custom - search custom validators
+					var customValidator = _.findWhere(customValidators, {name: validator});
+					if (!customValidator) {
+						return console.log('No validator of that sort - please register: "' + validator + '" by using Origin.scaffold.addCustomValidator(name, validatorMethod)');
+					}
+					// If match is found - add the method
+					validators.push(customValidator.validatorMethod);
+				}
+			}
+		});
+
+		var fieldObject = {
+			type: field.inputType,
+			validators: validators,
+			help: field.help,
+			default: field.default,
+			fieldType: field.inputType
+		};
+
+		if (_.isObject(field.inputType)) {
+			fieldObject = _.extend(fieldObject, field.inputType);
+		}
+
+		scaffoldSchema[key] = fieldObject;
+	};
+
+	var setupObjectSchemaField = function(field, key, schema, scaffoldSchema) {
+		scaffoldSchema[key] = {
+			type: 'Object',
+			subSchema: field.properties
+		}
+
+		var objectSchema = (schema[key].properties || schema[key].subSchema);
+		var scaffoldObjectSchema = scaffoldSchema[key].subSchema;
+
+		_.each(objectSchema, function(field, key) {
+			setupSchemaFields(field, key, objectSchema, scaffoldObjectSchema);
+		});
+	};
 
 	var buildSchema = function(schema, options, type) {
-
 		if (builtSchemas[type]) {
 			return builtSchemas[type];
 		}
-		
+
 		var scaffoldSchema = {};
-		
+
+		// Build schema
 		_.each(schema, function(field, key) {
-			// Build schema
-			setupSchemaFields(field, key, schema, scaffoldSchema);
-			
+			if("properties" === key && field.properties) {
+				_.each(field.properties, function(propField, propKey) {
+					setupSchemaFields(propField, propKey, field.properties, scaffoldSchema);
+				});
+			} else {
+				setupSchemaFields(field, key, schema, scaffoldSchema);
+			}
 		});
 
 		builtSchemas[type] = scaffoldSchema;
-
 		return scaffoldSchema;
-	}
+	};
 
-	var buildFieldsets = function(schema, options) {
+	/**
+	* 1. Merge both layout schemas
+	* 2. Extend the initial fieldsets and attempt to categorise
+	*
+	* TODO schema validation
+	* - Check everything in ordering has a corresponding fieldset (see extendLayoutFieldsets)
+	*/
+	//
+	var buildFieldSets = function(initialType, type, schema) {
+		// for base type
+		var initialLayout = Origin.editor.data.layouts[initialType];
+		var initialFieldsets = (initialLayout && initialLayout.fieldsets) ? initialLayout.fieldsets : false;
+		// for custom type
+		var layout = Origin.editor.data.layouts[type];
+		var fieldsets = (layout && layout.fieldsets) ? layout.fieldsets : false;
 
-		// Setup default fieldsets
+		if(fieldsets) {
+			if(initialType !== type && initialFieldsets) {
+				return extendLayoutFieldsets(initialLayout, layout);
+			}
+			else {
+				return fieldsets;
+			}
+		} else if(initialFieldsets) {
+			var newFields = getNewFieldsFromFieldsets(initialFieldsets, fieldsets);
+			var newLayout = {
+				fieldsets: generateNewFieldSets(schema, newFields)
+			};
+			return extendLayoutFieldsets(initialLayout, newLayout);
+		}
+		// NOTE assume that all base fieldsets will be supplied with core
+	};
+
+	var getFieldsFromFieldsets = function(fieldsets) {
+		var fields = [];
+		for(var key in fieldsets) {
+			fields = fields.concat(fieldsets[key].fields);
+		}
+		return fields;
+	};
+
+	var getNewFieldsFromFieldsets = function(initialFieldsets, extraFieldsets) {
+		var initialFields = getFieldsFromFieldsets(initialFieldsets);
+		var extraFields = getFieldsFromFieldsets(extraFieldsets);
+		var newFields = [];
+
+		for(var i = 0, len = extraFields.length; i < len; i++) {
+			if(-1 === initialFields.indexOf(extraFields[i])) {
+				newFields.push(extraFields[i]);
+			}
+		}
+
+		return newFields;
+	};
+
+	// create fieldsets from newFields
+	var generateNewFieldSets = function(schema, newFields) {
+		// TODO use localised strings here
 		var fieldsets = {
 			general: {
 				legend: 'General',
 				fields: []
 			},
-			// ::TODO
-			// I want to remove this please
-			properties: {
-				legend: 'Properties',
-				fields: []
-			}, 
 			settings :{
 				legend: 'Settings',
 				fields: []
-			},
-			extensions: {
-				legend: 'Extensions',
-				fields: ['_extensions']
 			}
 		};
 
-		_.each(schema, function(value, key) {
-			// Build main fieldSets
-			// Check if field is a settings
-			if (key === '_extensions') {
-				return;
+		for(var key in schema) {
+			if(-1 === newFields.indexOf(key) && 'properties' !== key) {
+				continue;
 			}
 
-			if (value.isSetting) {
-
+			// TODO do we have nested .isSettings?
+			if (schema[key].isSetting) {
 				fieldsets.settings.fields.push(key);
-
-			} else  if (value.type === 'object') {
-				// If value is an object - should give it some rights
-				// and add it as a field set - but not _extensions
-				if (fieldsets[key]) {
-
-					fieldsets[key].fields.push(key);
-
-				} else {
-
+			} else if (schema[key].type === 'object') {
+				debugger;
+				if(!fieldsets[key]) {
 					fieldsets[key] = {
 						legend: Helpers.keyToTitleString(key),
-						fields: [key]
+						fields: []
 					};
-
 				}
-
+				if(schema[key].properties) {
+					for(var propsKey in schema[key].properties) {
+						fieldsets[key].fields.push(propsKey);
+					}
+				} else {
+					fieldsets[key].fields.push(key);
+				}
 			} else {
-
-				// All general ones here please
 				fieldsets.general.fields.push(key);
-
 			}
-		});
-
-		// Should check if no settings and no extensions are available
-		if (!schema._extensions) {
-			delete fieldsets.extensions;
-		}
-		
-		// Delete empty field sets
-		if (fieldsets.settings.fields.length === 0) {
-			delete fieldsets.settings;
 		}
 
-		if (fieldsets.properties.fields.length === 0) {
-			delete fieldsets.properties;
+		for(var key in fieldsets) {
+			var isEmpty = (fieldsets[key].properties && 0 === Object.keys(fieldsets[key].properties).length) ||
+				0 === fieldsets[key].fields.length;
+			if(isEmpty) {
+				delete fieldsets[key];
+			}
 		}
 
-		// We only want the values
-		return _.values(fieldsets);
+		return fieldsets;
+	};
 
-	}
+	// NOTE add fieldsets from the 'extra' layout (any duplicates in the original are replaced)
+	var extendLayoutFieldsets = function(originalLayout, extraLayout) {
+		var extended = [];
+		var ordering = extraLayout.ordering || originalLayout.ordering;
+
+		// NOTE assumes that all ordering fieldsets actually exist
+		for(var i = 0, len = ordering.length; i < len; i++) {
+			extended.push(extraLayout.fieldsets[ordering[i]] || originalLayout.fieldsets[ordering[i]]);
+		}
+
+		// make sure any props in extra are added to the end
+		if(!extraLayout.ordering) {
+			var keys = Object.keys(extraLayout.fieldsets);
+			for(var i = 0, len = keys.length; i < len; i++) {
+				if(-1 === ordering.indexOf(keys[i])) {
+					extended.push(extraLayout.fieldsets[keys[i]]);
+				}
+			}
+		}
+
+		return extended;
+	};
+
+	/**
+	* Public static functions
+	*/
 
 	Scaffold.buildForm = function(options) {
-
-		// This shouldn't need to check whether this is not set
-		// _type:'config' should be set on the model
+		// TODO _type:'config' should be set on the model
 		var type = options.model.get('_type') || options.schemaType || 'config';
 		var initialType = type;
 
@@ -258,16 +314,13 @@ define(function(require) {
 			case 'component':
 				type = options.model.get('_component');
 				break;
-
 			case 'page':
 			case 'menu':
 				type = 'contentobject';
 				break;
-
 			case '_courseStyle':
 				type = 'course';
 				break;
-
 			case 'theme':
 				type = options.schemaType;
 				break;
@@ -275,20 +328,26 @@ define(function(require) {
 
 		var schema = new Schemas(type);
 
-		// Support ommission of attributes for certain types
-		switch (initialType) {
-			case '_courseStyle':
-				schema = _.pick(schema, 'customStyle');
-				break;
+		if('_courseStyle' === initialType) {
+			schema = _.pick(schema, 'customStyle');
 		}
-		
+
 		options.model.schema = buildSchema(schema, options, type);
-		options.fieldsets = buildFieldsets(schema, options);
+		options.fieldsets = buildFieldSets(initialType, type, schema);
+
+		if(!options.fieldsets) {
+			delete options.fieldsets;
+		}
+
 		currentModel = options.model;
+
 		var form = new Backbone.Form(options).render();
 		return form;
+	};
 
-	}
+	Scaffold.addEditorFieldset = function(jsonData) {
+
+	};
 
 	Scaffold.addCustomField = function(fieldName, view, overwrite) {
 		// Check if field already exists
@@ -297,8 +356,7 @@ define(function(require) {
 		} else {
 			Backbone.Form.editors[fieldName] = view;
 		}
-
-	}
+	};
 
 	Scaffold.addCustomValidator = function(name, validatorMethod) {
 		var customValidator = {};
@@ -331,7 +389,7 @@ define(function(require) {
 
 	Scaffold.getCurrentModel = function() {
 		return currentModel;
-	}
+	};
 
 	// Listen to modal views
 	Origin.on('scaffold:increaseActiveModals', function() {
@@ -344,26 +402,24 @@ define(function(require) {
 
 	Scaffold.getCurrentActiveModals = function() {
 		return ActiveItemsModal;
-	}
+	};
 
 	Scaffold.isOverlayActive = function() {
 		return isOverlayActive;
-	}
+	};
 
 	Scaffold.setOverlayActive = function(booleanValue) {
 		isOverlayActive = booleanValue;
-	}
+	};
 
 	// Example of customValidator
 	/*Scaffold.addCustomValidator('title', function(value, formValues) {
-
 		var err = {
             type: 'username',
             message: 'Usernames must be at least 3 characters long'
         };
 
         if (value.length < 3) return err;
-
 	});*/
 
 	Handlebars.registerHelper('console', function(context) {
@@ -371,5 +427,4 @@ define(function(require) {
 	});
 
 	Origin.scaffold = Scaffold;
-
 });

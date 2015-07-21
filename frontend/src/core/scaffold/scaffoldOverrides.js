6a3a67ace92c77aa0f068236c9bc9ba74fa443a9
Daryl Hedley <darylhedley@hotmail.com>
2015-07-21 15:11:39 +0100

    Adds an overwrite to backbone-forms-list
    Enabled arrays to display as countable items

--- frontend/src/core/scaffold/scaffoldOverrides.js
+++ frontend/src/core/scaffold/scaffoldOverrides.js
@@ -3,6 +3,7 @@ define(function(require) {
 
 	var Backbone = require('backbone');
 	var BackboneForms = require('backboneForms');
+	var BackboneFormsLists = require('backboneFormsLists');
 	var Handlebars = require('handlebars');
 
 	// Setup templates
@@ -26,6 +27,37 @@ define(function(require) {
 			fieldType: this.schema.fieldType
 		};
 	};
+
+	Backbone.Form.editors.List.Modal.prototype.itemToString = function(value) {
+      var createTitle = function(key) {
+        var context = { key: key };
+
+        return Backbone.Form.Field.prototype.createTitle.call(context);
+      };
+
+      value = value || {};
+
+      //Pretty print the object keys and values
+      var parts = [];
+      _.each(this.nestedSchema, function(schema, key) {
+        var desc = schema.title ? schema.title : createTitle(key),
+            val = value[key];
+
+        // If item is an object then print out how many items it contains
+        if (_.isArray(val)) {
+          var itemsString = ' items';
+          if (val.length === 1) {
+            itemsString = ' item';
+          }
+          val = val.length + itemsString;
+        }
+        if (_.isUndefined(val) || _.isNull(val)) val = '';
+
+        parts.push(desc + ': ' + val);
+      });
+
+      return parts.join('<br />');
+    };
 
 	// Used to setValue with defaults
 

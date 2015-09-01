7167cff3aec475a4e531a252c843680b96cb911e
Daryl Hedley <darylhedley@hotmail.com>
2015-09-01 15:48:04 +0100

    Few adjustments to custom editors catch

--- frontend/src/core/scaffold/scaffold.js
+++ frontend/src/core/scaffold/scaffold.js
@@ -68,7 +68,7 @@ define(function(require) {
 					scaffoldSchema[key] = fieldObject;
 				} else {
 					scaffoldSchema[key] = {
-						type: field.items.inputType || 'List',
+						type:  (Backbone.Form.editors[field.inputType]) ? field.items.inputType : 'List',
 						itemType: 'Object',
 						subSchema: field.items.properties,
 						fieldType: 'List'

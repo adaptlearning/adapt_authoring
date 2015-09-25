9ade9afd6c8161884437b7c619df4d26664fe297
Thomas Taylor <thomas.taylor@kineo.com>
2015-09-25 12:02:48 +0100

    Add toastr notify plugin

---
+++ frontend/src/plugins/notify-toastr/index.js
@@ -0,0 +1,27 @@
+// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
+define(function(require) {
+	var Origin = require('coreJS/app/origin');
+	var toastr = require('./toastr.min.js');
+
+	// TODO do this properly...
+	(function loadCss(url) {
+		var link = document.createElement("link");
+		link.type = "text/css";
+		link.rel = "stylesheet";
+		link.href = "/adaptbuilder/css/assets/toastr.min.css";
+		document.getElementsByTagName("head")[0].appendChild(link);
+	})();
+
+	toastr.options.progressBar = true;
+	toastr.options.timeOut = "5000";
+	toastr.options.hideDuration = "750";
+
+	var Toastr = function(data) {
+		if(!toastr[data.type]) console.log("Notify.toastr: invalid type '" + data.type + "'");
+		
+		if(data.callback) data.onHidden = data.callback;
+		toastr[data.type](data.text, data.title, data);
+	};
+
+	Origin.Notify.register('toastr', Toastr);
+});

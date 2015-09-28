fcf63a6b8de55b7954228aca34897afaee25e5e8
Thomas Taylor <thomas.taylor@kineo.com>
2015-09-28 10:28:31 +0100

    Rename toastr plugin to more generic toast

--- frontend/src/core/notify/plugins/toast/index.js
+++ frontend/src/core/notify/plugins/toast/index.js
@@ -1,7 +1,7 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
 	var Origin = require('coreJS/app/origin');
-	var toastr = require('./toastr.min');
+	var Toastr = require('./toastr.min');
 
 	// TODO do this properly...
 	(function loadCss(url) {
@@ -12,19 +12,19 @@ define(function(require) {
 		document.getElementsByTagName("head")[0].appendChild(link);
 	})();
 
-	toastr.options.progressBar = true;
-	toastr.options.timeOut = "5000";
-	toastr.options.hideDuration = "750";
+	Toastr.options.progressBar = false;
+	Toastr.options.timeOut = "5000";
+	Toastr.options.hideDuration = "750";
 
-	var Toastr = function(data) {
-		if(!toastr[data.type]) console.log("Notify.toastr: invalid type '" + data.type + "'");
+	var Toast = function(data) {
+		if(!Toastr[data.type]) console.log("Notify.toast: invalid type '" + data.type + "'");
 
 		if(data.callback) data.onHidden = data.callback;
-		toastr[data.type](data.text, data.title, data);
+		Toastr[data.type](data.text, data.title, data);
 	};
 
 	var init = function() {
-		Origin.Notify.register('toastr', Toastr);
+		Origin.Notify.register('toast', Toast);
 	};
 
 	return init;

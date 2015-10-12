2392f40d296d27c1e638f8231ce4f3fe8013c483
Brian Quinn <brian@learningpool.com>
2015-10-12 12:11:10 +0100

    Added generic error handler for preview route

--- routes/preview/index.js
+++ routes/preview/index.js
@@ -7,9 +7,19 @@ var Constants = require('../../lib/outputmanager').Constants;
 var usermanager = require('../../lib/usermanager');
 var helpers = require('../../lib/helpers');
 var logger = require('../../lib/logger');
+var methodOverride = require('method-override');
+var util = require('util');
 
+server.use(methodOverride());
 server.set('views', __dirname);
 server.set('view engine', 'hbs');
+
+function PreviewPermissionError(message, httpCode) {
+  this.message = message || "Permission denied";
+  this.http_code = httpCode || 401;
+}
+
+util.inherits(PreviewPermissionError, Error);
 
 server.get('/preview/:tenant/:course/*', function (req, res, next) {
   var courseId = req.params.course,
@@ -20,7 +30,8 @@ server.get('/preview/:tenant/:course/*', function (req, res, next) {
     
   if (!user) {
     logger.log('warn', 'Preview: Unauthorised attempt to view course %s on tenant %s', courseId, tenantId);
-    return res.status(501).end();
+    
+    return next(new PreviewPermissionError());
   }
   
   var options = {
@@ -31,14 +42,16 @@ server.get('/preview/:tenant/:course/*', function (req, res, next) {
     // Compare the tenantId values
     if (tenantId !== user.tenant._id.toString()) {
       logger.log('warn', 'Preview: User %s does not have permission to view course %s on tenant %s', user._id, courseId, tenantId);
-      return res.status(404).end();
+      
+      return next(new PreviewPermissionError());
     }    
 
     // When requesting the first page, check the user has access to this course.
     helpers.hasCoursePermission('*', user._id, tenantId, {_id: courseId}, function(err, hasPermission) {
       if (err) {
         logger.log('error', err);
-        return res.status(404).end();
+        
+        return next(new PreviewPermissionError());
       }
       
       // Verify the session is configured to hold the course previews accessible for this user.
@@ -55,7 +68,8 @@ server.get('/preview/:tenant/:course/*', function (req, res, next) {
         }
         
         logger.log('warn', 'Preview: User %s does not have permission to view course %s on tenant %s', user._id, courseId, tenantId);
-        return res.status(404).end();
+
+        return next(new PreviewPermissionError());
       }
       
       // Store in the session that the user has access to this course.
@@ -66,27 +80,28 @@ server.get('/preview/:tenant/:course/*', function (req, res, next) {
           // Display the error to the user.
           res.status(err.status).end();
         }
-        // else {
-        //   console.log('Sent:', file);
-        // }
       });
     });
   } else {
     // Verify that the user has appropriate access to the requested file.
     if (req.session.previews && req.session.previews.indexOf(courseId) > -1) {
-      
       res.sendFile(file, options, function(err) {
         if (err) {
           // Display the error to the user.
           // We don't want to clog the log with 404s, etc.
           res.status(err.status).end();
         }
-        // else {
-        //   console.log('Sent:', file);
-        // }
       });
     } else {
       return res.status(404).end();
     }
   }
 });
+
+function errorHandler(err, req, res, next) {
+  var httpCode = err.http_code || 500;
+  res.status(httpCode);
+  res.render('error', {http_code: httpCode, error: err});
+}
+
+server.use(errorHandler);

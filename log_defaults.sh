c40f099243a655698185d87ba5f91f580376e7c4
Thomas Berger <thomas.berger@learnchamp.com>
2015-11-25 14:12:35 +0100

    log default username and password

---
+++ log_defaults.sh
@@ -0,0 +1,5 @@
+echo ---------------------------------------------------------
+echo Adapt Authoring Tool is running on: http://localhost:5000
+echo username: admin
+echo password: password
+echo ---------------------------------------------------------

c97ff13443a1252f632e90dc5fe9d31d90e55f77
Thomas Taylor <thomas.taylor@kineo.com>
2016-06-07 16:00:27 +0100

    Switch all editor views to validate() form
    Rather than commit(), which doesn't return validation errors.

--- frontend/src/core/project/views/projectDetailView.js
+++ frontend/src/core/project/views/projectDetailView.js
@@ -33,13 +33,14 @@ define(function(require) {
     },
 
     saveProject: function(event) {
-      var errors = this.form.commit();
+      var errors = this.form.validate();
       // This must trigger no matter what, as sidebar needs to know
       // when the form has been resubmitted
       Origin.trigger('editorSidebar:showErrors', errors);
       if (errors) {
         return;
       }
+      this.form.commit();
 
       // Fix tags
       var tags = [];

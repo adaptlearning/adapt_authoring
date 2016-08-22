068fb7af44b84c54dbeae560d3b866e6f9078f23
Thomas Taylor <thomas.taylor@kineo.com>
2016-08-22 15:45:01 +0100

    Standardise Helpers line breaks

--- frontend/src/core/app/helpers.js
+++ frontend/src/core/app/helpers.js
@@ -8,18 +8,23 @@ define(function(require){
         console: function(context) {
           return console.log(context);
         },
+
         lowerCase: function(text) {
           return text.toLowerCase();
         },
+
         numbers: function(index) {
           return index +1;
         },
+
         capitalise:  function(text) {
           return text.charAt(0).toUpperCase() + text.slice(1);
         },
+
         odd: function (index) {
           return (index +1) % 2 === 0  ? 'even' : 'odd';
         },
+
         stringToClassName: function(text) {
           if (!text) {
             return;
@@ -32,6 +37,7 @@ define(function(require){
           // Remove _ and spaces with dashes
           return text.replace(/_| /g, "-").toLowerCase();
         },
+
         keyToTitleString: function(key) {
           if (!key) {
             return;
@@ -40,29 +46,29 @@ define(function(require){
           var string = key.replace(/_/g, "").toLowerCase();
           return this.capitalise(string);
         },
+
         formatDate: function(timestamp, noZero) {
           var noDisplay = '-';
           // 2014-02-17T17:00:34.196Z
           if (typeof(timestamp) !== 'undefined') {
             var date = new Date(timestamp);
-
             // optionally use noDisplay char if 0 dates are to be interpreted as such
             if (noZero && 0 === date.valueOf()) {
               return noDisplay;
             }
-
             return date.toDateString();
           }
 
           return noDisplay;
         },
+
         momentFormat: function(date, format) {
           if (typeof date == 'undefined') {
             return '-';
           }
-          
           return moment(date).format(format);
         },
+
         formatDuration: function(duration) {
           var zero = '0', hh, mm, ss;
           var time = new Date(0, 0, 0, 0, 0, Math.floor(duration), 0);
@@ -78,6 +84,7 @@ define(function(require){
 
           return hh + ':' + mm + ':' + ss;
         },
+
         // checks for http/https and www. prefix
         isAssetExternal: function(url) {
           if (url && url.length > 0) {
@@ -87,6 +94,7 @@ define(function(require){
             return true;
           }
         },
+
         ifValueEquals: function(value, text, block) {
             if (value === text) {
                 return block.fn(this);
@@ -94,6 +102,7 @@ define(function(require){
                 return block.inverse(this);
             }
         },
+
         ifUserIsMe: function(userId, block) {
           if (userId === Origin.sessionModel.get('id')) {
             return block.fn(this);
@@ -101,6 +110,7 @@ define(function(require){
             return block.inverse(this);
           }
         },
+
         selected: function(option, value){
             if (option === value) {
                 return ' selected';
@@ -108,27 +118,32 @@ define(function(require){
                 return ''
             }
         },
+
         counterFromZero: function(n, block) {
             var sum = '';
             for (var i = 0; i <= n; ++i)
                 sum += block.fn(i);
             return sum;
         },
+
         counterFromOne: function(n, block) {
             var sum = '';
             for (var i = 1; i <= n; ++i)
                 sum += block.fn(i);
             return sum;
         },
+
         t: function(str, options) {
             for (var placeholder in options.hash) {
               options[placeholder] = options.hash[placeholder];
             }
             return (window.polyglot != undefined ? window.polyglot.t(str, options) : str);
         },
+
         stripHtml: function(html) {
             return new Handlebars.SafeString(html);
         },
+
         bytesToSize: function(bytes) {
             if (bytes == 0) return '0 B';
 
@@ -138,6 +153,7 @@ define(function(require){
 
             return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
         },
+
         renderBooleanOptions: function(selectedValue) {
             var options = ["true", "false"];
             var html = '';
@@ -149,6 +165,7 @@ define(function(require){
 
             return new Handlebars.SafeString(html);
         },
+
         pickCSV: function (list, key, separator) {
           var vals = [];
           separator = (separator && separator.length) ? separator : ',';
@@ -163,6 +180,7 @@ define(function(require){
           }
           return vals.join(separator);
         },
+
         renderTags: function(list, key) {
           var html = '';
 
@@ -173,10 +191,8 @@ define(function(require){
               var tag = (key && list[i][key]) ?
                 list[i][key]
                 : list[i];
-
               html += '<li class="tag-item" title="' + tag + '"><span class="tag-value">' + tag  + '</span></li>';
             }
-
             html += '</ul>'
           }
 
@@ -202,7 +218,7 @@ define(function(require){
           var fileName = urlSplit[urlSplit.length - 1];
           // Get courseAsset model
           var courseAsset = Origin.editor.data.courseAssets.findWhere({_fieldName: fileName});
-          
+
           if (courseAsset) {
             var courseAssetId = courseAsset.get('_assetId');
 
@@ -221,7 +237,6 @@ define(function(require){
         },
 
         getThumbnailFromValue: function(url) {
-
           var urlSplit = url.split('/')
           var fileName = urlSplit[urlSplit.length - 1];
           // Get courseAsset model
@@ -253,47 +268,45 @@ define(function(require){
         },
 
         copyStringToClipboard: function(data) {
-                 
           var textArea = document.createElement("textarea");
-          
+
           textArea.value = data;
 
           // Place in top-left corner of screen regardless of scroll position.
           textArea.style.position = 'fixed';
           textArea.style.top = 0;
           textArea.style.left = 0;
-    
+
           // Ensure it has a small width and height. Setting to 1px / 1em
           // doesn't work as this gives a negative w/h on some browsers.
           textArea.style.width = '2em';
           textArea.style.height = '2em';
-    
+
           // We don't need padding, reducing the size if it does flash render.
           textArea.style.padding = 0;
-    
+
           // Clean up any borders.
           textArea.style.border = 'none';
           textArea.style.outline = 'none';
           textArea.style.boxShadow = 'none';
-    
+
           // Avoid flash of white box if rendered for any reason.
           textArea.style.background = 'transparent';
-    
+
           document.body.appendChild(textArea);
-    
+
           textArea.select();
-    
+
           var success = document.execCommand('copy');
 
           document.body.removeChild(textArea);
-          
+
           return success;
         },
-        
+
         validateCourseContent: function(currentCourse) {
           // Let's do a standard check for at least one child object
           var containsAtLeastOneChild = true;
-
           var alerts = [];
 
           function iterateOverChildren(model) {
@@ -314,7 +327,6 @@ define(function(require){
                 + "' with no "
                 + model._children
               );
-
               return;
             } else {
               // Go over each child and call validation again
@@ -322,7 +334,6 @@ define(function(require){
                 iterateOverChildren(childModel);
               });
             }
-
           }
 
           iterateOverChildren(currentCourse);
@@ -348,8 +359,6 @@ define(function(require){
         if (isConfirmed) {
           Origin.trigger('editor:courseValidation');
         }
-      }
-
     };
 
     for(var name in helpers) {

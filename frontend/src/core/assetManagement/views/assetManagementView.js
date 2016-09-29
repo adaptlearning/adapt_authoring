57cbfed6e89f16b7e7b94ee84e34807d2ff9af5b
Thomas Taylor <thomas.taylor@kineo.com>
2016-09-29 12:49:59 +0100

    Start redesign of asset management look/feel

--- frontend/src/core/assetManagement/views/assetManagementView.js
+++ frontend/src/core/assetManagement/views/assetManagementView.js
@@ -1,69 +1,59 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
-
   var Backbone = require('backbone');
-  var Handlebars = require('handlebars');
   var OriginView = require('coreJS/app/views/originView');
   var Origin = require('coreJS/app/origin');
-  var AssetModel = require('coreJS/assetManagement/models/assetModel');
   var AssetManagementCollectionView = require('coreJS/assetManagement/views/assetManagementCollectionView');
   var AssetManagementPreviewView = require('coreJS/assetManagement/views/assetManagementPreviewView');
 
   var AssetManagementView = OriginView.extend({
-
     tagName: 'div',
-
     className: 'asset-management',
-
-    events: {
-    },
 
     preRender: function() {
         this.listenTo(Origin, 'window:resize', this.resizeAssetPanels);
-        this.listenTo(Origin, 'assetManagement:assetItemView:preview', this.onAssetClicked);
-        this.listenTo(Origin, 'assetManagement:assetPreviewView:delete', this.onAssetDeleted);
+        this.listenTo(Origin, 'assetManagement:assetItemView:preview', this.showPreview);
+        this.listenTo(Origin, 'assetManagement:assetPreviewView:delete', this.hidePreview);
     },
 
     postRender: function() {
         this.setupSubViews();
         this.resizeAssetPanels();
-        // Set imageReady
-        _.defer(_.bind(this.setupImageReady, this));
-    },
-
-    setupImageReady: function() {
-      this.$el.imageready(this.setViewToReady);
+        _.defer(_.bind(function() {
+          this.$el.imageready(this.setViewToReady);
+        }, this));
     },
 
     setupSubViews: function() {
-        // Push collection through to collection view
-        this.$('.asset-management-assets-container-inner').append(new AssetManagementCollectionView({collection: this.collection}).$el);
+        var collectionView = new AssetManagementCollectionView({ collection: this.collection });
+        this.$('.asset-management-assets-container-inner').append(collectionView.$el);
     },
 
     resizeAssetPanels: function() {
         var navigationHeight = $('.navigation').outerHeight();
         var locationTitleHeight = $('.location-title').outerHeight();
-        var windowHeight = $(window).height();
-        var actualHeight = windowHeight - (navigationHeight + locationTitleHeight);
+        var actualHeight = $(window).height() - (navigationHeight + locationTitleHeight);
         this.$('.asset-management-assets-container').height(actualHeight);
         this.$('.asset-management-preview-container').height(actualHeight);
     },
 
-    onAssetClicked: function(model) {
-        this.$('.asset-management-no-preview').hide();
-        this.$('.asset-management-preview-container-inner').html(new AssetManagementPreviewView({
-            model: model
-        }).$el);
+    showPreview: function(model) {
+      var $container = this.$('.asset-management-preview-container-inner');
+      var animTime = 150;
+      this.$('.asset-management-preview-container').show();
+      this.$('.asset-management-no-preview').hide();
+      $container.fadeOut(animTime, function() {
+        var previewView = new AssetManagementPreviewView({ model: model });
+        $container.html(previewView.$el).fadeIn(animTime);
+      });
     },
 
-    onAssetDeleted: function() {
-        this.$('.asset-management-no-preview').show();
+    hidePreview: function() {
+      this.$('.asset-management-preview-container').hide();
     }
-    
   }, {
     template: 'assetManagement'
   });
 
   return AssetManagementView;
-
 });

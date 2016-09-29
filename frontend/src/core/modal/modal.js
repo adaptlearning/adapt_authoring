93c8f2430daea328d41903379c7cfcbfd3fe590b
Thomas Taylor <thomas.taylor@kineo.com>
2016-09-29 18:01:07 +0100

    Remove seemingly unused file

--- frontend/src/core/modal/modal.js
+++ frontend/src/core/modal/modal.js
@@ -1,9 +1,7 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-
   var Origin = require('coreJS/app/origin');
   var ModalView = require('coreJS/modal/views/modalView');
-  var ModalModel = require('coreJS/modal/models/modalModel');
 
   Origin.on('modal:open', function(view, options, context) {
     new ModalView({
@@ -12,5 +10,4 @@ define(function(require) {
       context: context
     });
   });
-
 });

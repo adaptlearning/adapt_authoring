76351b7f5412061b0e5994143cb3c7c4a4d029ee
Thomas Taylor <thomas.taylor@kineo.com>
2016-10-10 22:23:45 +0100

    Fix bug with superToolbar not initialising properly

--- frontend/src/plugins/superToolbar/index.js
+++ frontend/src/plugins/superToolbar/index.js
@@ -3,7 +3,7 @@ define(function(require) {
   var Origin = require('coreJS/app/origin');
   var SuperToolbarView = require('./views/SuperToolbarView.js');
 
-  Origin.on('app:dataReady', function() {
+  Origin.on('app:dataReady login:changed', function() {
     if (Origin.permissions.hasPermissions(["*/*:create","*/*:read","*/*:update","*/*:delete"])) {
       var toolbar = new SuperToolbarView();
       Origin.on('superToolbar:add', function(buttons) {

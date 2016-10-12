b932c88132202ddac8cd91025db34d057a3334ab
Thomas Taylor <thomas.taylor@kineo.com>
2016-10-12 11:05:00 +0100

    Fix whitespace

--- frontend/src/core/editor/extensions/views/editorExtensionsEditView.js
+++ frontend/src/core/editor/extensions/views/editorExtensionsEditView.js
@@ -19,110 +19,108 @@ define(function(require) {
     },
 
     preRender: function() {
-        this.currentSelectedIds = [];
-        this.listenTo(Origin, 'editorExtensionsEdit:views:add', this.addExtension);
-        this.listenTo(Origin, 'editorExtensionsEdit:views:remove', this.removeExtension);
-        this.setupExtensions();
+      this.currentSelectedIds = [];
+      this.listenTo(Origin, 'editorExtensionsEdit:views:add', this.addExtension);
+      this.listenTo(Origin, 'editorExtensionsEdit:views:remove', this.removeExtension);
+      this.setupExtensions();
     },
 
     setupExtensions: function() {
-        var availableExtensionsCollection = Origin.editor.data.extensionTypes;
-        var enabledExtensionsCollection = new Backbone.Collection(null, { comparator: 'displayName' });
-        var disabledExtensionsCollection = new Backbone.Collection(null, { comparator: 'displayName' });
+      var availableExtensionsCollection = Origin.editor.data.extensionTypes;
+      var enabledExtensionsCollection = new Backbone.Collection(null, { comparator: 'displayName' });
+      var disabledExtensionsCollection = new Backbone.Collection(null, { comparator: 'displayName' });
 
-        var enabledExtensionNames = _.pluck(Origin.editor.data.config.get('_enabledExtensions'), 'name');
+      var enabledExtensionNames = _.pluck(Origin.editor.data.config.get('_enabledExtensions'), 'name');
 
-        // sort into appropriate collection
-        availableExtensionsCollection.each(function(extension) {
-            if (_.indexOf(enabledExtensionNames, extension.get('name')) > -1) {
-                enabledExtensionsCollection.add(extension);
-            } else if(extension.get('_isAvailableInEditor')) {
-                disabledExtensionsCollection.add(extension);
-            }
-        });
+      // sort into appropriate collection
+      availableExtensionsCollection.each(function(extension) {
+        if (_.indexOf(enabledExtensionNames, extension.get('name')) > -1) {
+          enabledExtensionsCollection.add(extension);
+        } else if(extension.get('_isAvailableInEditor')) {
+          disabledExtensionsCollection.add(extension);
+        }
+      });
 
-        // Set collections on model render for render
-        this.model.set('enabledExtensions', enabledExtensionsCollection.toJSON());
-        this.model.set('availableExtensions', disabledExtensionsCollection.toJSON());
+      // Set collections on model render for render
+      this.model.set('enabledExtensions', enabledExtensionsCollection.toJSON());
+      this.model.set('availableExtensions', disabledExtensionsCollection.toJSON());
 
-        this.render();
-
-        _.defer(_.bind(this.postRender, this));
-
+      this.render();
+      // TODO is defer a good idea?
+      _.defer(_.bind(this.postRender, this));
     },
 
     postRender: function() {
-        this.setViewToReady();
+      this.setViewToReady();
     },
 
     postData: function(url) {
-        $.post(url + this.model.get('_id'), { extensions: this.currentSelectedIds }, _.bind(function(result) {
-            if (result.success) {
-                this.refreshData();
-            } else {
-                Origin.Notify.alert({
-                  type: 'error',
-                  text: window.polyglot.t('app.errorgeneric')
-                });
-            }
-        }, this));
+      $.post(url + this.model.get('_id'), { extensions: this.currentSelectedIds }, _.bind(function(result) {
+        if (result.success) {
+          this.refreshData();
+        } else {
+          Origin.Notify.alert({
+            type: 'error',
+            text: window.polyglot.t('app.errorgeneric')
+          });
+        }
+      }, this));
     },
 
     refreshData: function() {
-      var self = this;
       var configModel = new EditorConfigModel({ _courseId: this.model.get('_id') });
       // Ensure that the latest config model is always up-to-date when entering this screen
       configModel.fetch({
-        success: function(model, response, options) {
+        success: _.bind(function(model, response, options) {
           Origin.editor.data.config =  model;
           Origin.trigger('scaffold:updateSchemas', function() {
-            self.setupExtensions();
+            this.setupExtensions();
           }, this);
-        }
+        }, this);
       });
     },
 
     onAddExtensionClicked: function(event) {
-        this.currentSelectedIds = [$(event.currentTarget).attr('data-id')];
-        var extensionName = $(event.currentTarget).attr('data-displayname');
+      this.currentSelectedIds = [$(event.currentTarget).attr('data-id')];
+      var extensionName = $(event.currentTarget).attr('data-displayname');
 
-        Origin.Notify.confirm({
-          title: window.polyglot.t('app.manageextensions'),
-          text: window.polyglot.t('app.confirmapplyextension', { extension: extensionName }),
-          html: true,
-          callback: _.bind(this.onAddExtensionConfirmed, this)
-        });
+      Origin.Notify.confirm({
+        title: window.polyglot.t('app.manageextensions'),
+        text: window.polyglot.t('app.confirmapplyextension', { extension: extensionName }),
+        html: true,
+        callback: _.bind(this.onAddExtensionConfirmed, this)
+      });
     },
 
     onAddExtensionConfirmed: function(confirmed) {
-        if(confirmed) {
-            Origin.trigger('editorExtensionsEdit:views:add');
-        }
+      if(confirmed) {
+        Origin.trigger('editorExtensionsEdit:views:add');
+      }
     },
 
     addExtension: function() {
-        this.postData('/api/extension/enable/');
+      this.postData('/api/extension/enable/');
     },
 
     removeExtension: function() {
-        this.postData('/api/extension/disable/');
+      this.postData('/api/extension/disable/');
     },
 
     onRemoveExtensionClicked: function(event) {
-        this.currentSelectedIds = [$(event.currentTarget).attr('data-id')];
+      this.currentSelectedIds = [$(event.currentTarget).attr('data-id')];
 
-        Origin.Notify.confirm({
-          type: 'warning',
-          title: window.polyglot.t('app.deleteextension'),
-          text: window.polyglot.t('app.confirmdeleteextension'),
-          callback: _.bind(this.onRemoveExtensionConfirmed, this)
-        });
+      Origin.Notify.confirm({
+        type: 'warning',
+        title: window.polyglot.t('app.deleteextension'),
+        text: window.polyglot.t('app.confirmdeleteextension'),
+        callback: _.bind(this.onRemoveExtensionConfirmed, this)
+      });
     },
 
     onRemoveExtensionConfirmed: function(confirmed) {
-        if(confirmed) {
-            Origin.trigger('editorExtensionsEdit:views:remove');
-        }
+      if(confirmed) {
+        Origin.trigger('editorExtensionsEdit:views:remove');
+      }
     }
   },
   {

b3692a9a4c0613ada62ea581158f5b25a21a498c
Thomas Taylor <thomas.taylor@kineo.com>
2016-10-25 12:32:07 +0100

    Fix up import form

--- frontend/src/core/courseImport/views/courseImportView.js
+++ frontend/src/core/courseImport/views/courseImportView.js
@@ -8,6 +8,15 @@ define(function(require){
     tagName: 'div',
     className: 'courseImport',
 
+    events: {
+      'change input[name=file]': 'onFileSelected',
+      'dragenter label[for=file]': 'onDrag',
+      'dragover label[for=file]': 'onDrag',
+      'dragleave label[for=file]': 'onDrop',
+      'dragend label[for=file]': 'onDrop',
+      'drop label[for=file]': 'onDrop'
+    },
+
     preRender: function() {
       //events
       Origin.on('courseImport:import', this.submit, this);
@@ -22,7 +31,7 @@ define(function(require){
     submit: function() {
       // HACK in case the browser file filter doesn't work
       // (FF, I'm looking at you - https://bugzilla.mozilla.org/show_bug.cgi?id=826176)
-      var isZip = this.$('.import-form input[type="file"]').val().search(/.(?:\.zip$)/) > -1;
+      var isZip = this.$('form input[type="file"]').val().search(/.(?:\.zip$)/) > -1;
       if(!isZip) {
         return this.showError();
       } else {
@@ -30,7 +39,7 @@ define(function(require){
       }
 
       // TODO localise notify strings
-      this.$('.import-form').ajaxSubmit({
+      this.$('form').ajaxSubmit({
         error: function(data, status, error) {
           Origin.Notify.alert({
             type: 'error',
@@ -53,11 +62,40 @@ define(function(require){
     },
 
     showError: function(errorText) {
-      this.$('.import-form .error').removeClass('display-none');
+      this.$('label[for=file]').addClass('validation-error');
     },
+
     hideError: function(errorText) {
-      this.$('.import-form .error').addClass('display-none');
-    }
+      this.$('label[for=file]').removeClass('validation-error');
+    },
+
+    /*
+    * Events
+    */
+
+    onDrag: function(e) {
+      e && e.preventDefault();
+      e && e.stopPropagation();
+      this.$('label[for=file]').addClass('over');
+    },
+
+    onDrop: function(e) {
+      e && e.preventDefault();
+      e && e.stopPropagation();
+      this.$('label[for=file]').removeClass('over');
+      if(e.type === 'drop') {
+        var files = e.originalEvent.dataTransfer.files;
+        this.$('input[id=file]').prop('files', files);
+      }
+    },
+
+    onFileSelected: function(event) {
+      // Default 'title' -- remove C:\fakepath if it is added
+      var title = this.$('input[name=file]')[0].value.replace("C:\\fakepath\\", "");
+      // change upload button label
+      this.$('label[for=file] .btn-label').html(title);
+      this.$('label[for=file]').removeClass('validation-error').addClass('selected');
+    },
   }, {
     template: 'courseImport'
   });

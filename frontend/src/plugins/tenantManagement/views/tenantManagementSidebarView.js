40cc4f6ee8d4731d28ea42eefb11fdb80ad50bef
Ambadas Avhad <ambadas.avhad@vyassys.com>
2016-11-30 21:27:14 +0530

    Tenant management MVP
    Tenant management MVP

---
+++ frontend/src/plugins/tenantManagement/views/tenantManagementSidebarView.js
@@ -0,0 +1,18 @@
+define(function(require) {
+  var Origin = require('coreJS/app/origin');
+  var SidebarItemView = require('coreJS/sidebar/views/sidebarItemView');
+
+  var TenantManagementSidebarView = SidebarItemView.extend({
+    events: {
+      'click button.add': 'addTenant'
+    },
+
+    addTenant: function(event) {
+      event && event.preventDefault();
+      Origin.router.navigate('#/tenantManagement/addTenant');
+    }
+  }, {
+    template: 'tenantManagementSidebar'
+  });
+  return TenantManagementSidebarView;
+});

a6d6699ed52c5880c03f724255e653f131fce7bc
Thomas Taylor <thomas.taylor@kineo.com>
2017-01-05 16:36:21 +0000

    Remove themeSettings & menuSettings from new course page
    Fixes #160

--- frontend/src/core/scaffold/schemas.js
+++ frontend/src/core/scaffold/schemas.js
@@ -105,10 +105,10 @@ define(function(require) {
             delete schema._extensions;
             // Remove globals as these are appended to the course model
             delete schema.globals;
+            // assumption that no menu or theme applied, so don't show settings
+            delete schema.themeSettings;
+            delete schema.menuSettings;
 
-            if(!schema.themeSettings.properties) delete schema.themeSettings;
-            if(!schema.menuSettings.properties) delete schema.menuSettings;
-            
             return schema;
         }
     };

a18dd8cd1fe3d91a0ff86fede24005f8eb55e994
Dan Gray <dan@sinensis.co.uk>
2017-02-27 11:38:16 +0000

    Import front end plugin

---
+++ frontend/src/plugins/courseImport/views/courseImportView.js
@@ -0,0 +1,143 @@
+// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
+define(function(require){
+  var Helpers = require('coreJS/app/helpers');
+  var Origin = require('coreJS/app/origin');
+  var OriginView = require('coreJS/app/views/originView');
+  var TagsInput = require('core/libraries/jquery.tagsinput.min');
+
+  var CourseImportView = OriginView.extend({
+    tagName: 'div',
+    className: 'courseImport',
+    createdCourseId: false,
+
+    preRender: function() {
+      Origin.trigger('location:title:update', { title: window.polyglot.t('app.courseImportTitle') });
+      this.listenTo(Origin, 'courseImport:uploadCourse', this.uploadCourse);
+    },
+
+    postRender: function() {
+      // tagging
+      this.$('#tags_control').tagsInput({
+        autocomplete_url: '/api/autocomplete/tag',
+        onAddTag: _.bind(this.onAddTag, this),
+        onRemoveTag: _.bind(this.onRemoveTag, this),
+        'minChars' : 3,
+        'maxChars' : 30
+      });
+      this.setViewToReady();
+    },
+
+    isValid: function() {
+      var reqs = this.$('.required');
+      var uploadFile = this.$('.asset-file');
+      var validated = true;
+      var uploadFileErrormsg = $(uploadFile).prev('label').find('span.error');
+      $.each(reqs, function (index, el) {
+        var errormsg = $(el).prev('label').find('span.error');
+        if (!$.trim($(el).val())) {
+          validated = false;
+          $(el).addClass('input-error');
+          $(errormsg).text(window.polyglot.t('app.pleaseentervalue'));
+        } else {
+          $(el).removeClass('input-error');
+          $(errormsg).text('');
+        }
+      });
+
+      if (this.model.isNew() && !uploadFile.val()) {
+        validated = false;
+        $(uploadFile).addClass('input-error');
+        $(uploadFileErrormsg).text(window.polyglot.t('app.pleaseaddfile'));
+      } else {
+        $(uploadFile).removeClass('input-error');
+        $(uploadFileErrormsg).text('');
+      }
+      return validated;
+    },
+
+    uploadCourse: function() {
+      if(!this.isValid()) {
+        return;
+      }
+
+      var tags = [];
+      _.each(this.model.get('tags'), function (item) {
+        item._id && tags.push(item._id);
+      });
+      this.$('#tags').val(tags);
+
+      // submit form data
+      this.$('form.courseImport').ajaxSubmit({
+
+        uploadProgress: function(event, position, total, percentComplete) {
+          $(".progress-container").css("visibility", "visible");
+          var percentVal = percentComplete + '%';
+          $(".progress-bar").css("width", percentVal);
+          $('.progress-percent').html(percentVal);
+        },
+
+        error: _.bind(this.onAjaxError, this),
+        success: _.bind(this.onFormSubmitSuccess, this)
+      });
+
+      return false;
+    },
+
+    goBack: function() {
+      Origin.router.navigate('#/dashboard', { trigger:true });
+    },
+
+    onFormSubmitSuccess: function(importData, impoprtStatus, importXhr) {
+      this.createdCourseId = importData._id;
+
+      var self = this;
+      //var chosenRole = $('form.courseImport select[name=role]').val();
+    },
+
+    onAjaxSuccess: function() {
+      this.goBack();
+    },
+
+    onAjaxError: function(data, status, error) {
+      // We may have a partially created course, make sure it's gone
+      if(this.createdCourseId) {
+        // TODO - add route for course destroy
+        //$.ajax('/api/course/' + this.createdCourseId, { method: 'DELETE', error: _.bind(this.onAjaxError, this) });
+      }
+      Origin.Notify.alert({
+        type: 'error',
+        title: window.polyglot.t('app.importerrortitle'),
+        text: data.responseText || error
+      });
+    },
+
+    onAddTag: function (tag) {
+      var model = this.model;
+      $.ajax({
+        url: '/api/content/tag',
+        method: 'POST',
+        data: { title: tag }
+      }).done(function (data) {
+        if (data && data._id) {
+          var tags = model.get('tags') || [];
+          tags.push({ _id: data._id, title: data.title });
+          model.set({ tags: tags });
+        }
+      });
+    },
+
+    onRemoveTag: function (tag) {
+      var tags = [];
+      _.each(this.model.get('tags'), function (item) {
+        if (item.title !== tag) {
+          tags.push(item);
+        }
+      });
+      this.model.set({ tags: tags });
+    }
+  }, {
+    template: 'courseImport'
+  });
+
+  return CourseImportView;
+});

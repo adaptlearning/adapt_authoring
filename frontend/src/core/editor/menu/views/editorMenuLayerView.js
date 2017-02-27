04ea5887d5d003926a9ed986ef562885a2a8be9d
Tom Taylor <hello@tomtaylor.name>
2017-02-27 10:52:15 +0000

    Fix editorMenuLayerView whitespace

--- frontend/src/core/editor/menu/views/editorMenuLayerView.js
+++ frontend/src/core/editor/menu/views/editorMenuLayerView.js
@@ -1,34 +1,31 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-
-	var Origin = require('coreJS/app/origin');
+  var Origin = require('coreJS/app/origin');
+	var EditorArticleModel = require('editorPage/models/editorArticleModel');
+	var EditorBlockModel = require('editorPage/models/editorBlockModel');
+	var EditorContentObjectModel = require('editorMenu/models/editorContentObjectModel');
+	var EditorMenuItemView = require('editorMenu/views/editorMenuItemView');
   var EditorOriginView = require('editorGlobal/views/editorOriginView');
-  var EditorContentObjectModel = require('editorMenu/models/editorContentObjectModel');
-  var EditorArticleModel = require('editorPage/models/editorArticleModel');
-  var EditorBlockModel = require('editorPage/models/editorBlockModel');
-  var EditorMenuItemView = require('editorMenu/views/editorMenuItemView');
 
   var EditorMenuLayerView = EditorOriginView.extend({
-
       className: 'editor-menu-layer',
 
       events: {
-        'click button.editor-menu-layer-add-page' : 'addPage',
-        'click button.editor-menu-layer-add-menu' : 'addMenu',
-        'click .editor-menu-layer-paste'          : 'pasteMenuItem',
-        'click .editor-menu-layer-paste-cancel'   : 'cancelPasteMenuItem'
+        'click button.editor-menu-layer-add-page': 'addPage',
+        'click button.editor-menu-layer-add-menu': 'addMenu',
+        'click .editor-menu-layer-paste': 'pasteMenuItem',
+        'click .editor-menu-layer-paste-cancel': 'cancelPasteMenuItem'
       },
 
       preRender: function(options) {
         if (options._parentId) {
           this._parentId = options._parentId;
         }
-
         this.listenTo(Origin, 'editorView:removeSubViews', this.remove);
         this.listenTo(Origin, 'editorMenuView:removeMenuViews', this.remove);
       },
 
-  		postRender: function() {
+      postRender: function() {
         if (this._parentId) {
           // Append the parentId value to the container to allow us to move pages, etc.
           this.$el.attr('data-parentId', this._parentId);
@@ -40,10 +37,7 @@ define(function(require) {
         var data = this.data ? this.data : false;
         var template = Handlebars.templates[this.constructor.template];
         this.$el.html(template(data));
-        _.defer(_.bind(function() {
-          this.postRender();
-        }, this));
-
+        _.defer(_.bind(this.postRender, this));
         return this;
       },
 
@@ -77,33 +71,21 @@ define(function(require) {
           displayTitle: (type == 'page'? window.polyglot.t('app.placeholdernewpage') : window.polyglot.t('app.placeholdernewmenu')),
           body: '',
           linkText: window.polyglot.t('app.view'),
-          graphic: {
-            alt: '',
-            src: ''
-          },
+          graphic: { alt: '', src: '' },
           _type: type
         });
-
         // Instantly add the view for UI purposes
         var newMenuItemView = this.addMenuItemView(newMenuItemModel);
-
         // Save the model
         newMenuItemModel.save(null, {
           error: function(error) {
-            // If there's an error show the menu item fading out
-            // and show an unobtrusive pop notification
-            var timeOut = 3000;
+            // If there's an error show the menu item fading out and alert
             newMenuItemView.$el.removeClass('syncing').addClass('not-synced');
-
             Origin.Notify.alert({
               type: 'error',
               text: window.polyglot.t('app.errormenueditorbody'),
             });
-
-            _.delay(function() {
-              newMenuItemView.remove();
-            }, timeOut);
-
+            _.delay(newMenuItemView.remove, 3000);
           },
           success: _.bind(function(model) {
             Origin.editor.data.contentObjects.add(model);
@@ -116,19 +98,15 @@ define(function(require) {
             } else {
               newMenuItemView.$el.removeClass('syncing').addClass('synced');
             }
-
             this.setHeight();
-
           }, this)
         });
       },
 
       addNewPageArticleAndBlock: function(model, newMenuItemView) {
-
         var typeToAdd;
         var newChildModel;
         var newChildTitle;
-
         this.pageModel;
         this.pageView;
 
@@ -152,17 +130,12 @@ define(function(require) {
           _courseId: Origin.editor.data.course.get('_id')
         }, {
           error: function() {
-            var timeOut = 3000;
             newMenuItemView.$el.removeClass('syncing').addClass('not-synced');
-
             Origin.Notify.alert({
               type: 'error',
               text: window.polyglot.t('app.errormenueditorbody'),
             });
-
-            _.delay(function() {
-              newMenuItemView.remove();
-            }, timeOut);
+            _.delay(newMenuItemView.remove, 3000);
           },
           success: _.bind(function(model, response, options) {
             // Add this new element to the collect
@@ -173,15 +146,12 @@ define(function(require) {
             } else {
               newMenuItemView.$el.removeClass('syncing').addClass('synced');
             }
-
           }, this)
         });
       },
 
       addMenuItemView: function(model) {
-        var newMenuItemView = new EditorMenuItemView({
-          model: model
-        });
+        var newMenuItemView = new EditorMenuItemView({ model: model });
         this.$('.editor-menu-layer-inner').append(newMenuItemView.$el.addClass('syncing'));
         return newMenuItemView;
       },
@@ -189,9 +159,6 @@ define(function(require) {
       pasteMenuItem: function(event) {
         event.preventDefault();
         Origin.trigger('editorView:paste', this._parentId, this.$('.editor-menu-item').length + 1);
-        /*_.delay(_.bind(function() {
-          Origin.trigger('editorView:menuView:updateSelectedItem', this);
-        }, this), 2000)*/
       },
 
       cancelPasteMenuItem: function(event) {
@@ -204,11 +171,9 @@ define(function(require) {
         });
         Origin.trigger('editorView:pasteCancel', target);
       }
-
-  	}, {
-  		template: 'editorMenuLayer'
+    }, {
+      template: 'editorMenuLayer'
   });
 
   return EditorMenuLayerView;
-
 });

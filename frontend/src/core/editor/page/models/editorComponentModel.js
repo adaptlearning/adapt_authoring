9052b3ed1cd1e675b46d97d6b87ce9b7dc45064e
lc-thomasberger <thomas.berger@learnchamp.com>
2017-03-27 11:50:37 +0200

    implement onScreen support (#1490)
    * update model schemas
    
    * Added `_onScreen` attribute to the whitelist so that it's saved. Articles and ContentObjects do not use whitelisting.
    
    * minor typo fixes
    
    * wording update

--- frontend/src/core/editor/page/models/editorComponentModel.js
+++ frontend/src/core/editor/page/models/editorComponentModel.js
@@ -13,7 +13,7 @@ define(function(require) {
         // These are the only attributes which should be permitted on a save
         whitelistAttributes: ['_id', '_componentType', '_courseId', '_layout', '_parentId', 
             '_type', 'properties', '_component', '_extensions', '_classes', '_isOptional', '_isAvailable', 'body', 'displayTitle', 
-            'title', 'version']
+            'title', 'version', '_onScreen']
     });
 
     return EditorComponentModel;

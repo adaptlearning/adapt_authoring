a6464532dc8e57e6d60aea22c166f02178e174d8
Tom Taylor <hello@tomtaylor.name>
2017-03-28 16:08:20 +0100

    Refactor config.js

--- frontend/src/core/config.js
+++ frontend/src/core/config.js
@@ -1,118 +1,106 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 require.config({
-    baseUrl: '../../',
-    paths: {
-      jquery: 'core/libraries/jquery',
-      underscore: 'core/libraries/underscore',
-      backbone: 'core/libraries/backbone',
-      modernizr: 'core/libraries/modernizr',
-      handlebars: 'core/libraries/handlebars',
-      inview: 'core/libraries/inview',
-      imageReady: 'core/libraries/imageReady',
-      templates: 'templates/templates',
-      polyglot: 'core/libraries/polyglot.min',
-      sweetalert: 'core/libraries/sweetalert.min',
-      backboneForms:'core/libraries/backbone-forms',
-      backboneFormsLists:'core/libraries/backbone-forms-lists',
-      jsoneditor: 'core/libraries/jquery.jsoneditor.min',
-      'jquery-ui': 'core/libraries/jquery-ui.min',
-      'jquery-form' : 'core/libraries/jquery.form',
-      typeahead: 'core/libraries/typeahead',
-      velocity: 'core/libraries/velocity',
-      scrollTo: 'core/libraries/scrollTo',
-      colorPicker: 'core/libraries/colorPicker/js/colorpicker',
-      'mediaelement' : 'core/libraries/mediaelement-and-player',
-      editorPage: 'core/editor/page',
-      editorMenu: 'core/editor/menu',
-      editorCourse: 'core/editor/course',
-      editorConfig: 'core/editor/config',
-      editorTheme: 'core/editor/theme',
-      editorMenuSettings: 'core/editor/menuSettings',
-      editorGlobal: 'core/editor/global',
-      editorExtensions: 'core/editor/extensions',
-      tags: 'core/libraries/jquery.tagsinput.min',
-      ace: 'core/libraries/ace',
-      pikaday: 'core/libraries/pikaday/js/pikaday',
-      moment: 'core/libraries/moment.min'
+  paths: {
+    // third-party libs
+    ace: 'core/libraries/ace',
+    backbone: 'core/libraries/backbone',
+    backboneForms:'core/libraries/backbone-forms',
+    backboneFormsLists:'core/libraries/backbone-forms-lists',
+    colorPicker: 'core/libraries/colorPicker/js/colorpicker',
+    handlebars: 'core/libraries/handlebars',
+    imageReady: 'core/libraries/imageReady',
+    inview: 'core/libraries/inview',
+    jquery: 'core/libraries/jquery',
+    'jquery-form' : 'core/libraries/jquery.form',
+    'jquery-ui': 'core/libraries/jquery-ui.min',
+    jsoneditor: 'core/libraries/jquery.jsoneditor.min',
+    mediaelement : 'core/libraries/mediaelement-and-player',
+    modernizr: 'core/libraries/modernizr',
+    moment: 'core/libraries/moment.min',
+    pikaday: 'core/libraries/pikaday/js/pikaday',
+    polyglot: 'core/libraries/polyglot.min',
+    scrollTo: 'core/libraries/scrollTo',
+    sweetalert: 'core/libraries/sweetalert.min',
+    tags: 'core/libraries/jquery.tagsinput.min',
+    typeahead: 'core/libraries/typeahead',
+    underscore: 'core/libraries/underscore',
+    velocity: 'core/libraries/velocity',
+    // internal aliases
+    templates: 'templates/templates'
+  },
+  shim: {
+    // third-party
+    ace: {
+      exports: 'ace/ace'
     },
-    shim: {
-      jquery: [
-
-      ],
-      backbone: {
-          deps: [
-            'underscore',
-            'jquery'
-          ],
-          exports: 'Backbone'
-      },
-      underscore: {
-          exports: '_'
-      },
-      handlebars: {
-        exports: 'Handlebars'
-      },
-      templates: {
-        deps:['handlebars']
-      },
-      polyglot: {
-        exports: 'Polyglot'
-      },
-      sweetalert: {
-        deps: ['jquery'],
-        exports: 'sweetAlert'
-      },
-      jsoneditor: {
-        deps: ['jquery'],
-        exports: 'JsonEditor'
-      },
-      moment: {
-        exports: 'moment'
-      },
-      'mediaelement': {
-        deps: ['jquery'],
-        exports: 'mediaelement'
-      },
-      velocity: {
-        deps: ['jquery'],
-        exports: 'velocity'
-      },
-      scrollTo: {
-        deps: ['jquery'],
-        exports: 'scrollTo'
-      },
-      'jquery-ui': {
-        deps: ['jquery'],
-        exports: "$"
-      },
-      'jquery-form': {
-        deps: ['jquery'],
-        exports: "$"
-      },
-      backboneForms: {
-        deps: ['backbone']
-      },
-      backboneFormsLists: {
-        deps: ['backboneForms']
-      },
-      inview: {
-        deps: [
-          'jquery'
-        ],
-        exports: 'inview'
-      },
-      imageReady: {
-        deps: [
-          'jquery'
-        ],
-        exports: 'imageready'
-      },
-      ace: {
-        exports: 'ace/ace'
-      },
-      tags: {
-        deps: ['jquery'],
-        exports: "$"
-      }
+    backbone: {
+      deps: ['underscore','jquery'],
+      exports: 'Backbone'
+    },
+    backboneForms: {
+      deps: ['backbone']
+    },
+    backboneFormsLists: {
+      deps: ['backboneForms']
+    },
+    colorPicker: {
+      deps: ['jquery'],
+    },
+    handlebars: {
+      exports: 'Handlebars'
+    },
+    imageReady: {
+      deps: ['jquery'],
+      exports: 'imageready'
+    },
+    inview: {
+      deps: ['jquery'],
+      exports: 'inview'
+    },
+    'jquery-form': {
+      deps: ['jquery'],
+      exports: "$"
+    },
+    'jquery-ui': {
+      deps: ['jquery'],
+      exports: "$"
+    },
+    jsoneditor: {
+      deps: ['jquery'],
+      exports: 'JsonEditor'
+    },
+    mediaelement: {
+      deps: ['jquery'],
+      exports: 'mediaelement'
+    },
+    moment: {
+      exports: 'moment'
+    },
+    polyglot: {
+      exports: 'Polyglot'
+    },
+    scrollTo: {
+      deps: ['jquery'],
+      exports: 'scrollTo'
+    },
+    sweetalert: {
+      deps: ['jquery'],
+      exports: 'sweetAlert'
+    },
+    tags: {
+      deps: ['jquery'],
+      exports: "$"
+    },
+    underscore: {
+      exports: '_'
+    },
+    velocity: {
+      deps: ['jquery'],
+      exports: 'velocity'
+    },
+    // internal
+    templates: {
+      deps:['handlebars']
     }
+  }
 });

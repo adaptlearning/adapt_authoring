99219323f2bbad7230571a469e821f6ee7198717
Tom Taylor <hello@tomtaylor.name>
2017-04-03 10:28:36 +0100

    Refactor options files

--- frontend/src/modules/options/index.js
+++ frontend/src/modules/options/index.js
@@ -1,20 +1,13 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-// A module that offers options inside the title bar
-
 define(function(require) {
+  var Origin = require('core/origin');
+  var OptionsView = require('./views/optionsView');
 
-	var Origin = require('core/origin');
-	var OptionsView = require('./views/optionsView');
+  var Options = {
+    addItems: function(items) {
+      $('#app').prepend(new OptionsView({ collection: new Backbone.Collection(items) }).$el);
+    }
+  };
 
-	var Options = {};
-
-	Options.addItems = function(items) {
-
-		var itemsCollection = new Backbone.Collection(items);
-		$('#app').prepend(new OptionsView({collection:itemsCollection}).$el);
-
-	}
-
-	Origin.options = Options;
-
+  Origin.options = Options;
 });
--- frontend/src/modules/options/views/optionsView.js
+++ frontend/src/modules/options/views/optionsView.js
@@ -1,125 +1,89 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
+  var Origin = require('core/origin');
+  var Backbone = require('backbone');
 
-	var Origin = require('core/origin');
-	var Backbone = require('backbone');
+  var OptionsView = Backbone.View.extend({
+    className: 'options',
 
-	var OptionsView = Backbone.View.extend({
+    events: {
+    'click a': 'onOptionClicked'
+    },
 
-		className: 'options',
+    initialize: function() {
+      this.eventsToTrigger = [];
 
-		initialize: function() {
-			this.eventsToTrigger = [];
-			this.listenTo(Origin, 'remove:views', this.remove);
-			this.listenTo(Origin, 'options:update:ui', this.updateUI);
-			this.render();
-		},
+      this.listenTo(Origin, {
+        'remove:views': this.remove,
+        'options:update:ui': this.updateUI
+      });
+      this.render();
+    },
 
-		events: {
-			'click a': 'onOptionClicked'
-		},
+    render: function() {
+      var template = Handlebars.templates[this.constructor.template];
+      this.$el.html(template());
+      _.defer(_.bind(this.postRender, this));
+      return this;
+    },
 
-		render: function() {
-		    var template = Handlebars.templates[this.constructor.template];
-		    this.$el.html(template());
-		    _.defer(_.bind(function() {
-		    	this.postRender();
-		    }, this));
-		    return this;
-		},
+    renderOptions: function() {
+      var template = Handlebars.templates['optionsItem'];
+      // Go through each item and check if it has a group
+      // If it does - render into that group
+      this.collection.each(function(item) {
+        if (_.indexOf(this.eventsToTrigger, item.get('callbackEvent')) > -1) {
+          item.set('selected', true);
+        }
+        var itemGroup = item.get('group')
+        var parentSelector = (itemGroup) ? '.options-group-' + itemGroup : '.options-inner';
+        $(parentSelector).append(template(item.toJSON()));
+      }, this);
+    },
 
-		updateUI: function(userPreferences) {
-			// Wait until current render has finished
-			// Then go through each preference and add a class
-			_.defer(_.bind(function() {
-				_.each(userPreferences, function(preference) {
-					// If preferences are not classes to be added
-					// return this ship home
-					if (_.isArray(preference)) {
-						return;
-					}
-					this.$('a.option-value-' + preference).addClass('selected');
-				}, this)
-			}, this));
-			
-		},
+    postRender: function() {
+      this.sortAndRenderGroups();
+      // Add a defer to make sure the groups are rendered
+      _.defer(_.bind(this.renderOptions, this));
+    },
 
-		postRender: function() {
-			// First sort out the groups
-			this.sortAndRenderGroups();
-			// Add a defer to make sure the groups are rendered
-			_.defer(_.bind(this.renderOptions, this));
+    updateUI: function(userPreferences) {
+      // set selected preferences
+      _.defer(_.bind(function() {
+        _.each(userPreferences, function(preference) {
+          if (_.isArray(preference)) return;
+          this.$('a.option-value-' + preference).addClass('selected');
+        }, this);
+      }, this));
+    },
 
-		},
+    sortAndRenderGroups: function() {
+      var availableGroups = _.uniq(this.collection.pluck('group'));
+      var template = Handlebars.templates['optionsGroup'];
+      _.each(availableGroups, function(group) {
+        this.$('.options-inner').append(template({ group: group }));
+      });
+    },
 
-		sortAndRenderGroups: function() {
-			// Find all possible groups
-			var groups = this.collection.pluck('group');
-			var availableGroups = _.uniq(groups);
+    setSelectedOption: function(selectedOption) {
+      var group = selectedOption.attr('data-group');
+      var callbackEvent = selectedOption.attr('data-callback');
+      // If this item is in a group toggle all group items to not selected
+      if(!group || selectedOption.hasClass('selected')) {
+        return;
+      }
+      this.$('.options-group-' + group + ' a').removeClass('selected');
+      selectedOption.addClass('selected');
+      Origin.trigger(callbackEvent);
+    },
 
-			var template = Handlebars.templates['optionsGroup'];
+    onOptionClicked: function(event) {
+      event && event.preventDefault();
+      this.setSelectedOption($(event.currentTarget));
+    }
+  }, {
+    template: 'options'
+  });
 
-			_.each(availableGroups, function(group) {
-				this.$('.options-inner').append(template({group:group}));
-			});
-			
-		},
-
-		renderOptions: function() {
-
-			var template = Handlebars.templates['optionsItem'];
-
-			// Go through each item and check if it has a group
-			// If it does - render into that group
-			this.collection.each(function(item) {
-				
-				var itemGroup = item.get('group')
-				var $parent = this.$('.options-inner');
-
-				if (_.indexOf(this.eventsToTrigger, item.get('callbackEvent')) > -1) {
-					item.set('selected', true);	
-				}
-
-				var data = item.toJSON();
-
-				if (itemGroup) {
-					$parent = this.$('.options-group-' + itemGroup);
-				}
-				$parent.append(template(data));
-
-			}, this);
-
-		},
-
-		setSelectedOption: function(selectedOption) {
-			var group = selectedOption.attr('data-group');
-			var callbackEvent = selectedOption.attr('data-callback');
-
-			// If this item is in a group toggle all group items to not selected
-			if (group && !selectedOption.hasClass('selected')) {
-
-				this.$('.options-group-' + group + ' a').removeClass('selected');
-				selectedOption.addClass('selected');
-				Origin.trigger(callbackEvent);
-				
-			} else {
-			// How should we handle items not in groups? Should they be toggles?	
-			}
-
-		},
-
-		onOptionClicked: function(event) {
-			event.preventDefault();
-
-			var $selectedOption = $(event.currentTarget);
-
-			this.setSelectedOption($selectedOption);
-		}
-
-	}, {
-		template: 'options'
-	});
-
-	return OptionsView;
-
+  return OptionsView;
 });

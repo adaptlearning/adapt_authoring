3efee836293026b5728b3655b984ffad7a518252
Tom Taylor <hello@tomtaylor.name>
2017-04-06 18:10:57 +0100

    Refactor sessionModel

--- frontend/src/modules/user/models/sessionModel.js
+++ frontend/src/modules/user/models/sessionModel.js
@@ -1,73 +1,50 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-define(function(require) {
-
-  var Backbone = require('backbone');
-  var Origin = require('core/origin');
-
+define(['require', 'backbone', 'core/origin'], function(require, Backbone, Origin) {
   var SessionModel = Backbone.Model.extend({
-
+    url: "/api/authcheck",
     defaults: {
-      isAuthenticated: false,
       id: '',
       tenantId: '',
       email: '',
-      otherLoginLinks: []
+      isAuthenticated: false,
+      permissions: [],
+      otherLoginLinks: [],
+      _canRevert: false
     },
 
-    url: "/api/authcheck",
-
-    initialize: function() {},
-
-    logout: function () {
-      var self = this;
-
-      $.post(
-        '/api/logout',
-        function() {
-          self.set('isAuthenticated', false);
-          self.set('id', '');
-          self.set('tenantId', '');
-          self.set('email', '');
-          self.set('permissions', []);
-          self.set('_canRevert', false);
-
-          Origin.trigger('login:changed');
-          Origin.router.navigateToLogin();
-      });
+    initialize: function() {
     },
 
     login: function (username, password, shouldPersist) {
-      var self = this;
-
-      $.ajax({
-        method: 'post',
-        url: '/api/login',
-        data: {email: username, password: password, shouldPersist: shouldPersist},
-        success: function (jqXHR, textStatus, errorThrown) {
-          if (jqXHR.success) {
-            self.set('id', jqXHR.id);
-            self.set('tenantId', jqXHR.tenantId);
-            self.set('email', jqXHR.email);
-            self.set('isAuthenticated', jqXHR.success);
-            self.set('permissions', jqXHR.permissions);
-
-            Origin.trigger('login:changed');
-            Origin.trigger('schemas:loadData', Origin.router.navigateToHome);
-          }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          var errorCode = 1;
-
-          if (jqXHR.responseJSON && jqXHR.responseJSON.errorCode) {
-            errorCode = jqXHR.responseJSON.errorCode;
-          }
-
-          Origin.trigger('login:failed', errorCode);
-        }
+      var postData = {
+        email: username,
+        password: password,
+        shouldPersist: shouldPersist
+      };
+      $.post('/api/login', postData, _.bind(function (jqXHR, textStatus, errorThrown) {
+        this.set({
+          id: jqXHR.id,
+          tenantId: jqXHR.tenantId,
+          email: jqXHR.email,
+          isAuthenticated: true,
+          permissions: jqXHR.permissions
+        });
+        Origin.trigger('login:changed');
+        Origin.trigger('schemas:loadData', Origin.router.navigateToHome);
+      }, this)).fail(function(jqXHR, textStatus, errorThrown) {
+        Origin.trigger('login:failed', (jqXHR.responseJSON && jqXHR.responseJSON.errorCode) || 1);
       });
-    }
+    },
+
+    logout: function () {
+      $.post('/api/logout', _.bind(function() {
+        // revert to the defaults
+        this.set(this.defaults);
+        Origin.trigger('login:changed');
+        Origin.router.navigateToLogin();
+      }, this));
+    },
   });
 
   return SessionModel;
-
 });

7c7a31d5a23047a85ed87981677cb1bcca6e8b31
Tom Taylor <hello@tomtaylor.name>
2017-05-10 16:05:18 +0100

    Add backbone forms import

--- frontend/src/modules/scaffold/views/scaffoldTagsView.js
+++ frontend/src/modules/scaffold/views/scaffoldTagsView.js
@@ -1,7 +1,7 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-
     var Backbone = require('backbone');
+    var BackboneForms = require('backboneForms');
     var Origin = require('core/origin');
     var Helpers = require('core/helpers');
 

9ea41249fd003b2f4b5c7d42fc53abbca64a9415
Tom Taylor <hello@tomtaylor.name>
2017-08-16 21:15:44 +0100

    Move import to modules folder

--- frontend/src/modules/courseImport/index.js
+++ frontend/src/modules/courseImport/index.js
@@ -1,6 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Origin = require('coreJS/app/origin');
+  var Origin = require('core/origin');
   var CourseImportView = require('./views/courseImportView.js');
   var CourseImportSidebarView = require('./views/courseImportSidebarView.js');
 
@@ -10,13 +10,13 @@ define(function(require) {
     featurePermissions: ["*/*:create","*/*:read","*/*:update","*/*:delete"]
   };
 
-  Origin.on('app:dataReady login:changed', function() {
+  Origin.on('origin:dataReady login:changed', function() {
     Origin.permissions.addRoute('courseImport', data.featurePermissions);
 
   	if (Origin.permissions.hasPermissions(data.featurePermissions)) {
   		Origin.globalMenu.addItem({
         "location": "global",
-        "text": window.polyglot.t('app.courseimportmenu'),
+        "text": Origin.l10n.t('app.courseimportmenu'),
         "icon": "fa-upload",
         "sortOrder": 5,
         "callbackEvent": "courseImport:open"
@@ -27,21 +27,15 @@ define(function(require) {
   });
 
   Origin.on('globalMenu:courseImport:open', function() {
-    Origin.router.navigate('#/courseImport', {trigger: true});
+    Origin.router.navigateTo('courseImport');
   });
 
   Origin.on('router:courseImport', function(location, subLocation, action) {
-
-    onRoute(location, subLocation, action);
-
-  });
-
-  var onRoute = function(location, subLocation, action) {
     var mainView, sidebarView;
     mainView = CourseImportView;
     sidebarView = CourseImportSidebarView;
 
     Origin.router.createView(mainView, { model: new Backbone.Model({ globalData: data }) });
     Origin.sidebar.addView(new sidebarView().$el);
-  };
+  });
 });
--- frontend/src/modules/courseImport/views/courseImportSidebarView.js
+++ frontend/src/modules/courseImport/views/courseImportSidebarView.js
@@ -1,7 +1,7 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Origin = require('coreJS/app/origin');
-  var SidebarItemView = require('coreJS/sidebar/views/sidebarItemView');
+  var Origin = require('core/origin');
+  var SidebarItemView = require('modules/sidebar/views/sidebarItemView');
 
   var CourseImportSidebarView = SidebarItemView.extend({
     events: {

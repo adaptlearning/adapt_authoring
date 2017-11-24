9e932b025d6617483137081aa4491dc2e11989fe
Thomas Taylor <thomas.taylor@kineo.com>
2017-11-24 14:07:40 +0000

    Fix indentation

--- frontend/src/modules/assetManagement/index.js
+++ frontend/src/modules/assetManagement/index.js
@@ -14,35 +14,32 @@ define(function(require) {
     Origin.assetManagement.filterData = {};
 
     if (!location) {
-        var tagsCollection = new TagsCollection();
-
-        tagsCollection.fetch({
-          success: function() {
-            // Load asset collection before so sidebarView has access to it
-            var assetCollection = new AssetCollection();
-            // No need to fetch as the collectionView takes care of this
-            // Mainly due to serverside filtering
-            Origin.trigger('location:title:hide');
-            Origin.sidebar.addView(new AssetManagementSidebarView({collection: tagsCollection}).$el);
-            Origin.contentPane.setView(AssetManagementView, {collection: assetCollection});
-            Origin.trigger('assetManagement:loaded');
-          },
-          error: function() {
-            console.log('Error occured getting the tags collection - try refreshing your page');
-          }
-        });
-    } else if (location=== 'new') {
-        Origin.trigger('location:title:update', {title: 'New Asset'});
-        Origin.sidebar.addView(new AssetManagementNewAssetSidebarView().$el);
-        Origin.contentPane.setView(AssetManagementNewAssetView, { model: new AssetModel });
+      (new TagsCollection()).fetch({
+        success: function(tagsCollection) {
+          // Load asset collection before so sidebarView has access to it
+          var assetCollection = new AssetCollection();
+          // No need to fetch as the collectionView takes care of this
+          // Mainly due to serverside filtering
+          Origin.trigger('location:title:hide');
+          Origin.sidebar.addView(new AssetManagementSidebarView({ collection: tagsCollection }).$el);
+          Origin.contentPane.setView(AssetManagementView, { collection: assetCollection });
+          Origin.trigger('assetManagement:loaded');
+        },
+        error: function() {
+          console.log('Error occured getting the tags collection - try refreshing your page');
+        }
+      });
+    } else if (location === 'new') {
+      Origin.trigger('location:title:update', { title: 'New Asset' });
+      Origin.sidebar.addView(new AssetManagementNewAssetSidebarView().$el);
+      Origin.contentPane.setView(AssetManagementNewAssetView, { model: new AssetModel });
     } else if (subLocation === 'edit') {
-      var Asset = new AssetModel({ _id: location });
       // Fetch existing asset model
-      Asset.fetch({
-        success: function() {
-          Origin.trigger('location:title:update', {title: 'Edit Asset'});
+      (new AssetModel({ _id: location })).fetch({
+        success: function(model) {
+          Origin.trigger('location:title:update', { title: 'Edit Asset' });
           Origin.sidebar.addView(new AssetManagementNewAssetSidebarView().$el);
-          Origin.contentPane.setView(AssetManagementNewAssetView, { model: Asset });
+          Origin.contentPane.setView(AssetManagementNewAssetView, { model: model });
         }
       });
     }
--- frontend/src/modules/assetManagement/views/assetManagementView.js
+++ frontend/src/modules/assetManagement/views/assetManagementView.js
@@ -1,34 +1,27 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
-
-  var Backbone = require('backbone');
-  var Handlebars = require('handlebars');
-  var OriginView = require('core/views/originView');
   var Origin = require('core/origin');
-  var AssetModel = require('../models/assetModel');
+  var OriginView = require('core/views/originView');
   var AssetManagementCollectionView = require('./assetManagementCollectionView');
   var AssetManagementPreviewView = require('./assetManagementPreviewView');
 
   var AssetManagementView = OriginView.extend({
-
     tagName: 'div',
-
     className: 'asset-management',
 
-    events: {
-    },
-
     preRender: function() {
-        this.listenTo(Origin, 'window:resize', this.resizeAssetPanels);
-        this.listenTo(Origin, 'assetManagement:assetItemView:preview', this.onAssetClicked);
-        this.listenTo(Origin, 'assetManagement:assetPreviewView:delete', this.onAssetDeleted);
+      this.listenTo(Origin, {
+        'window:resize': this.resizeAssetPanels,
+        'assetManagement:assetItemView:preview': this.onAssetClicked,
+        'assetManagement:assetPreviewView:delete': this.onAssetDeleted
+      });
     },
 
     postRender: function() {
-        this.setupSubViews();
-        this.resizeAssetPanels();
-        // Set imageReady
-        _.defer(_.bind(this.setupImageReady, this));
+      this.setupSubViews();
+      this.resizeAssetPanels();
+      // Set imageReady
+      _.defer(_.bind(this.setupImageReady, this));
     },
 
     setupImageReady: function() {
@@ -36,34 +29,30 @@ define(function(require){
     },
 
     setupSubViews: function() {
-        // Push collection through to collection view
-        this.$('.asset-management-assets-container-inner').append(new AssetManagementCollectionView({collection: this.collection}).$el);
+      // Push collection through to collection view
+      this.$('.asset-management-assets-container-inner').append(new AssetManagementCollectionView({ collection: this.collection }).$el);
     },
 
     resizeAssetPanels: function() {
-        var navigationHeight = $('.navigation').outerHeight();
-        var locationTitleHeight = $('.location-title').outerHeight();
-        var windowHeight = $(window).height();
-        var actualHeight = windowHeight - (navigationHeight + locationTitleHeight);
-        this.$('.asset-management-assets-container').height(actualHeight);
-        this.$('.asset-management-preview-container').height(actualHeight);
+      var navigationHeight = $('.navigation').outerHeight();
+      var locationTitleHeight = $('.location-title').outerHeight();
+      var windowHeight = $(window).height();
+      var actualHeight = windowHeight - (navigationHeight + locationTitleHeight);
+      this.$('.asset-management-assets-container').height(actualHeight);
+      this.$('.asset-management-preview-container').height(actualHeight);
     },
 
     onAssetClicked: function(model) {
-        this.$('.asset-management-no-preview').hide();
-        this.$('.asset-management-preview-container-inner').html(new AssetManagementPreviewView({
-            model: model
-        }).$el);
+      this.$('.asset-management-no-preview').hide();
+      this.$('.asset-management-preview-container-inner').html(new AssetManagementPreviewView({ model: model }).$el);
     },
 
     onAssetDeleted: function() {
-        this.$('.asset-management-no-preview').show();
+      this.$('.asset-management-no-preview').show();
     }
-
   }, {
     template: 'assetManagement'
   });
 
   return AssetManagementView;
-
 });

7eb0858bbfa5ed016286d3cf1708fa97cb7eed06
Dan Gray <dan@sinensis.co.uk>
2018-03-10 10:54:46 +0000

    Change to ensureDirSync

--- install.js
+++ install.js
@@ -235,10 +235,7 @@ installHelpers.getLatestFrameworkVersion(function(error, latestFrameworkTag) {
   }
   console.log('');
   if(!fs.existsSync('conf/config.json')) {
-    fs.ensureDir('conf', function(err) {
-      console.error(err);
-    });
-
+    fs.ensureDirSync('conf');
     return start();
   }
   console.log('Found an existing config.json file. Do you want to use the values in this file during install?');

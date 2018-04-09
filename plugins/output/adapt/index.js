a290f4339390c94e2008f01f79606e933ee64ebe
Thomas Taylor <thomas.taylor@kineo.com>
2018-04-09 15:56:19 +0100

    Revert adapt plugin

--- plugins/output/adapt/index.js
+++ plugins/output/adapt/index.js
@@ -23,7 +23,6 @@ var origin = require('../../../'),
     assetmanager = require('../../../lib/assetmanager'),
     exec = require('child_process').exec,
     semver = require('semver'),
-    helpers = require('../../../lib/helpers'),
     installHelpers = require('../../../lib/installHelpers'),
     logger = require('../../../lib/logger');
 
@@ -35,13 +34,16 @@ util.inherits(AdaptOutput, OutputPlugin);
 AdaptOutput.prototype.publish = function(courseId, mode, request, response, next) {
   var app = origin();
   var self = this;
-  var user = usermanager.getCurrentUser();
-  var tenantId = user.tenant._id;
-  var outputJson = {};
-  var isRebuildRequired = false;
-  var themeName;
-  var menuName = Constants.Defaults.MenuName;
-  var frameworkVersion;
+  var user = usermanager.getCurrentUser(),
+    tenantId = user.tenant._id,
+    outputJson = {},
+    isRebuildRequired = false,
+    themeName = '',
+    menuName = Constants.Defaults.MenuName,
+    frameworkVersion;
+
+  var resultObject = {};
+
   // shorthand directories
   var FRAMEWORK_ROOT_FOLDER = path.join(configuration.tempDir, configuration.getConfig('masterTenantID'), Constants.Folders.Framework);
   var SRC_FOLDER = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.Source);
@@ -52,85 +54,119 @@ AdaptOutput.prototype.publish = function(courseId, mode, request, response, next
   var customPluginName = user._id;
 
   async.series([
-    function collateJSON(callback) {
-      self.getCourseJSON(tenantId, courseId, function(err, data) {
-        outputJson = data;
-        callback(err);
-      });
-    },
-    function doTheme(callback) {
-      var temporaryThemeFolder = path.join(SRC_FOLDER, Constants.Folders.Theme, customPluginName);
-      self.applyTheme(tenantId, courseId, outputJson, temporaryThemeFolder, function(err, appliedThemeName) {
-        outputJson.config[0]._theme = themeName = appliedThemeName;
-        callback(err);
-      });
-    },
-    function sanitizeJSON(callback) {
-      self.sanitizeCourseJSON(mode, outputJson, function(err, data) {
-        outputJson = data;
-        callback(err);
-      });
-    },
-    function checkRebuildRequired(callback) {
-      self.buildFlagExists(path.join(BUILD_FOLDER, Constants.Filenames.Rebuild), function(err, exists) {
-        isRebuildRequired = exists;
-        callback(err);
-      });
-    },
-    function doCustomStyle(callback) {
-      var temporaryThemeFolder = path.join(SRC_FOLDER, Constants.Folders.Theme, customPluginName);
-      self.writeCustomStyle(tenantId, courseId, temporaryThemeFolder, function(err, appliedThemeName) {
-        callback(err);
-      });
-    },
-    function doMenu(callback) {
-      var temporaryMenuFolder = path.join(SRC_FOLDER, Constants.Folders.Menu, customPluginName);
-      self.applyMenu(tenantId, courseId, outputJson, temporaryMenuFolder, function(err, appliedMenuName) {
-        menuName = appliedMenuName;
-        callback(err);
-      });
-    },
-    function writeAssets(callback) {
-      var assetsFolder = path.join(BUILD_FOLDER, Constants.Folders.Course, outputJson['config']._defaultLanguage, Constants.Folders.Assets);
-      self.writeCourseAssets(tenantId, courseId, assetsFolder, outputJson, function(err, modifiedJson) {
-        outputJson = modifiedJson;
-        callback(err);
-      });
-    },
-    function writeJSON(callback) {
-      self.writeCourseJSON(outputJson, path.join(BUILD_FOLDER, Constants.Folders.Course), callback);
-    },
-    function buildCourse(callback) {
-      fs.exists(path.join(BUILD_FOLDER, Constants.Filenames.Main), function(exists) {
-        if (exists && !isRebuildRequired) {
-          return callback();
-        }
-        logger.log('info', '3.1. Ensuring framework build exists');
-
-        installHelpers.getInstalledFrameworkVersion(function(error, frameworkVersion) {
-          var outputFolder = COURSE_FOLDER.replace(FRAMEWORK_ROOT_FOLDER + path.sep, '');
-          // Append the 'build' folder to later versions of the framework
-          // FIXME probably about time we removed this...
-          if (semver.gte(semver.clean(frameworkVersion), semver.clean('2.0.0'))) {
-            outputFolder = path.join(outputFolder, Constants.Folders.Build);
+      // get an object with all the course data
+      function(callback) {
+        self.getCourseJSON(tenantId, courseId, function(err, data) {
+          if (err) {
+            return callback(err);
           }
-          logger.log('info', `3.2. Using theme: ${themeName}`);
-          logger.log('info', `3.3. Using menu: ${menuName}`);
 
-          var buildMode = outputJson.config._generateSourcemap === true ? 'dev' : 'prod';
-          var cmd = `grunt server-build:${buildMode} --outputdir=${outputFolder} --theme=${themeName} --menu=${menuName}`;
+          // Store off the retrieved collections
+          outputJson = data;
 
-          logger.log('info', cmd);
+          callback(null);
+        });
+      },
+      //
+      function(callback) {
+        var temporaryThemeFolder = path.join(SRC_FOLDER, Constants.Folders.Theme, customPluginName);
+        self.applyTheme(tenantId, courseId, outputJson, temporaryThemeFolder, function(err, appliedThemeName) {
+          if (err) {
+            return callback(err);
+          }
 
-          exec(cmd, { cwd: FRAMEWORK_ROOT_FOLDER }, function(error, stdout, stderr) {
-            if (error !== null || stderr.length !== 0) {
-              if(error) {
-                logger.log('error', `exec error: ${error}`);
-                logger.log('error', `stdout error: ${stdout}`);
-              } else {
-                logger.log('error', `stderr: ${stderr}`);
-              }
-              return callback(error);
+          // Replace the theme in outputJson with the applied theme name.
+          themeName = appliedThemeName;
+
+          outputJson['config'][0]._theme = themeName;
+
+          callback(null);
+        });
+      },
+      function(callback) {
+        self.sanitizeCourseJSON(mode, outputJson, function(err, data) {
+          if (err) {
+            return callback(err);
+          }
+
+          // Update the JSON object
+          outputJson = data;
+
+          callback(null);
+        });
+      },
+      function(callback) {
+        self.buildFlagExists(path.join(BUILD_FOLDER, Constants.Filenames.Rebuild), function(err, exists) {
+          if (err) {
+            return callback(err);
+          }
+
+          isRebuildRequired = exists;
+
+          callback(null);
+        });
+      },
+      function(callback) {
+        var temporaryThemeFolder = path.join(SRC_FOLDER, Constants.Folders.Theme, customPluginName);
+        self.writeCustomStyle(tenantId, courseId, temporaryThemeFolder, function(err) {
+          if (err) {
+            return callback(err);
+          }
+
+          callback(null);
+        });
+      },
+      function(callback) {
+        var temporaryMenuFolder = path.join(SRC_FOLDER, Constants.Folders.Menu, customPluginName);
+        self.applyMenu(tenantId, courseId, outputJson, temporaryMenuFolder, function(err, appliedMenuName) {
+          if (err) {
+            return callback(err);
+          }
+
+          menuName = appliedMenuName;
+
+          callback(null);
+        });
+      },
+      function(callback) {
+        var assetsFolder = path.join(BUILD_FOLDER, Constants.Folders.Course, outputJson['config']._defaultLanguage, Constants.Folders.Assets);
+
+        self.writeCourseAssets(tenantId, courseId, assetsFolder, outputJson, function(err, modifiedJson) {
+          if (err) {
+            return callback(err);
+          }
+
+          // Store the JSON with the new paths to assets
+          outputJson = modifiedJson;
+
+          callback(null);
+        });
+      },
+      function(callback) {
+        self.writeCourseJSON(outputJson, path.join(BUILD_FOLDER, Constants.Folders.Course), function(err) {
+          if (err) {
+            return callback(err);
+          }
+          callback(null);
+        });
+      },
+      function(callback) {
+        installHelpers.getInstalledFrameworkVersion(function(error, version) {
+          frameworkVersion = version;
+          callback(error);
+        });
+      },
+      function(callback) {
+        fs.exists(path.join(BUILD_FOLDER, Constants.Filenames.Main), function(exists) {
+          if (!exists || isRebuildRequired) {
+            logger.log('info', '3.1. Ensuring framework build exists');
+
+            var args = [];
+            var outputFolder = COURSE_FOLDER.replace(FRAMEWORK_ROOT_FOLDER + path.sep,'');
+
+            // Append the 'build' folder to later versions of the framework
+            if (semver.gte(semver.clean(frameworkVersion), semver.clean('2.0.0'))) {
+              outputFolder = path.join(outputFolder, Constants.Folders.Build);
             }
 
             args.push('--outputdir=' + outputFolder);
@@ -187,8 +223,8 @@ AdaptOutput.prototype.publish = function(courseId, mode, request, response, next
       function(callback) {
         if (mode === Constants.Modes.publish) {
           // Now zip the build package
-          var filename = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.AllCourses, tenantId, courseId, Constants.Filenames.Download);
-          var zipName = helpers.slugify(outputJson['course'].title);
+          var filename = path.join(COURSE_FOLDER, Constants.Filenames.Download);
+          var zipName = self.slugify(outputJson['course'].title);
           var output = fs.createWriteStream(filename),
             archive = archiver('zip');
 
@@ -207,7 +243,11 @@ AdaptOutput.prototype.publish = function(courseId, mode, request, response, next
           });
 
           archive.pipe(output);
-          archive.glob('**/*', {cwd: path.join(BUILD_FOLDER)}).finalize();
+
+          archive.bulk([
+            { expand: true, cwd: path.join(BUILD_FOLDER), src: ['**/*'] },
+          ]).finalize();
+
         } else {
           // No download required -- skip this step
           callback();
@@ -218,39 +258,23 @@ AdaptOutput.prototype.publish = function(courseId, mode, request, response, next
         logger.log('error', err);
         return next(err);
       }
-      // Now zip the build package
-      var filename = path.join(COURSE_FOLDER, Constants.Filenames.Download);
-      var zipName = helpers.slugify(outputJson['course'].title);
-      var output = fs.createWriteStream(filename);
 
-      output.on('close', function() {
-        // Indicate that the zip file is ready for download
-        app.emit('zipCreated', tenantId, courseId, filename, zipName);
-        next(null, {
-          filename: filename,
-          zipName: zipName
-        });
-      });
-      archive.on('error', callback);
-      archive.pipe(output);
-      archive.bulk([ { expand: true, cwd: path.join(BUILD_FOLDER), src: ['**/*'] } ]).finalize();
-    }
-  ], function(err) {
-    if (err) {
-      logger.log('error', err);
-    }
-    next(err);
-  });
+      return next(null, resultObject);
+    });
+
 };
 
 AdaptOutput.prototype.export = function (courseId, request, response, next) {
   var self = this;
   var tenantId = usermanager.getCurrentUser().tenant._id;
-  var userId = usermanager.getCurrentUser()._id;
+  var timestamp = new Date().toISOString().replace('T', '-').replace(/:/g, '').substr(0,17);
 
   var FRAMEWORK_ROOT_FOLDER = path.join(configuration.tempDir, configuration.getConfig('masterTenantID'), Constants.Folders.Framework);
   var COURSE_ROOT_FOLDER = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.AllCourses, tenantId, courseId);
-  var exportDir = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.Exports, userId);
+
+  // set in getCourseName
+  var exportName;
+  var exportDir;
 
   var mode = Constants.Modes.export;
 
@@ -258,7 +282,27 @@ AdaptOutput.prototype.export = function (courseId, request, response, next) {
     function publishCourse(callback) {
       self.publish(courseId, mode, request, response, callback);
     },
-    function copyFiles(results, callback) {
+    function getCourseName(results, callback) {
+      database.getDatabase(function (error, db) {
+        if (error) {
+          return callback(err);
+        }
+
+        db.retrieve('course', { _id: courseId }, { jsonOnly: true }, function (error, results) {
+          if (error) {
+            return callback(error);
+          }
+          if(!results || results.length > 1) {
+            return callback(new Error('Unexpected results returned for course ' + courseId + ' (' + results.length + ')', self));
+          }
+
+          exportName = self.slugify(results[0].title) + '-export-' + timestamp;
+          exportDir = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.Exports, exportName);
+          callback();
+        });
+      });
+    },
+    function copyFiles(callback) {
       self.generateIncludesForCourse(courseId, function(error, includes) {
         if(error) {
           return callback(error);
@@ -304,21 +348,20 @@ AdaptOutput.prototype.export = function (courseId, request, response, next) {
     },
     function zipFiles(callback) {
       var archive = archiver('zip');
-      var zipPath = exportDir +  '.zip';
-      var output = fs.createWriteStream(zipPath);
+      var output = fs.createWriteStream(exportDir +  '.zip');
+
       archive.on('error', callback);
       output.on('close', callback);
       archive.pipe(output);
-      archive.glob('**/*', {cwd: exportDir}).finalize();
+      archive.bulk([{ expand: true, cwd: exportDir, src: ['**/*'] }]).finalize();
+    },
+    function cleanUp(callback) {
+      fse.remove(exportDir, function (error) {
+        callback(error, { zipName: exportName + '.zip' });
+      });
     }
   ],
-  function onDone(asyncError) {
-    // remove the exportDir, if there is one
-    fse.remove(exportDir, function(removeError) {
-      // async error more important
-      next(asyncError || removeError);
-    });
-  });
+  next);
 };
 
 /**

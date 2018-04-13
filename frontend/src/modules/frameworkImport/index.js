325e06a78d512e579f69231c3daa5976eccf4777
Thomas Taylor <thomas.taylor@kineo.com>
2018-04-13 12:39:18 +0100

    Move import button

--- frontend/src/modules/frameworkImport/index.js
+++ frontend/src/modules/frameworkImport/index.js
@@ -4,30 +4,12 @@ define(function(require) {
   var FrameworkImportView = require('./views/frameworkImportView.js');
   var FrameworkImportSidebarView = require('./views/frameworkImportSidebarView.js');
 
-// TODO - isReady and permissions
-  var isReady = true;
   var data = {
     featurePermissions: ["*/*:create","*/*:read","*/*:update","*/*:delete"]
   };
 
   Origin.on('origin:dataReady login:changed', function() {
     Origin.permissions.addRoute('frameworkImport', data.featurePermissions);
-
-  	if(!Origin.permissions.hasPermissions(data.featurePermissions)) {
-      isReady = true;
-      return;
-    }
-		Origin.globalMenu.addItem({
-      "location": "global",
-      "text": Origin.l10n.t('app.frameworkimportmenu'),
-      "icon": "fa-upload",
-      "sortOrder": 6,
-      "callbackEvent": "frameworkImport:open"
-    });
-  });
-
-  Origin.on('globalMenu:frameworkImport:open', function() {
-    Origin.router.navigate('#/frameworkImport', {trigger: true});
   });
 
   Origin.on('router:frameworkImport', function(location, subLocation, action) {

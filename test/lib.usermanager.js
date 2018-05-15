6cc6a0870db1e5eb2dd58e6c09d281e91ef5453d
Tom Greenfield <tom.greenfield@kineo.com>
2018-05-15 16:39:02 +0100

    Fix tests

--- test/lib.usermanager.js
+++ test/lib.usermanager.js
@@ -15,14 +15,11 @@ var userReset = {
 
 // ensure the user record is deleted
 after(function(done) {
-  usermanager.retrieveUser({ email: testData.user.email }, function(error, record) {
+  usermanager.deleteUser({ email: testData.user.email }, function(error) {
     if(error) return done(error);
-    usermanager.deleteUser(record, function(error) {
+    usermanager.retrieveUserPasswordReset({ token: userReset.token }, function(error, record) {
       if(error) return done(error);
-      usermanager.retrieveUserPasswordReset({ token: userReset.token }, function(error, record) {
-        if(error) return done(error);
-        usermanager.deleteUserPasswordReset({ user: record.id }, done);
-      });
+      usermanager.deleteUserPasswordReset({ user: record.id }, done);
     });
   });
 });

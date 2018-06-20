86702cf4c3edc49c58e05d6b2676f582236b24ef
Thomas Berger <thomas.berger@learnchamp.com>
2018-06-20 08:56:04 +0200

    remove unused code

--- frontend/src/modules/frameworkImport/views/frameworkImportView.js
+++ frontend/src/modules/frameworkImport/views/frameworkImportView.js
@@ -76,7 +76,6 @@ define(function(require){
     },
 
     onFormSubmitSuccess: function(importData, impoprtStatus, importXhr) {
-      this.createdCourseId = importData._id;
       Origin.router.navigateToHome();
     },
 
@@ -86,11 +85,6 @@ define(function(require){
 
     onAjaxError: function(data, status, error) {
       Origin.trigger('sidebar:resetButtons');
-      // We may have a partially created course, make sure it's gone
-      if(this.createdCourseId) {
-        // TODO - add route for course destroy
-        //$.ajax('/api/course/' + this.createdCourseId, { method: 'DELETE', error: _.bind(this.onAjaxError, this) });
-      }
       var text = (data.responseText) ? data.responseText.replace(/\n/g, "<br />") : error;
 
       Origin.Notify.alert({

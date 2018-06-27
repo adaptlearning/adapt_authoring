cb4913c98674224ce4789a9aa6ec70b879750d0e
Tom Taylor <hello@tomtaylor.name>
2018-06-27 10:01:13 +0100

    Add support for menu/theme settings
    Also reduce code duplication in general

--- frontend/src/modules/scaffold/schemas.js
+++ frontend/src/modules/scaffold/schemas.js
@@ -11,65 +11,46 @@ define([ 'core/origin', './models/schemasModel' ], function(Origin, SchemasModel
 
     if (!config) {
       delete schema._extensions;
-
       return schema;
     }
 
-    // remove any extensions and components that are not enabled on this course
     var enabledExtensions = _.pluck(config.get('_enabledExtensions'), 'targetAttribute');
-    var schemaExtensions = schema._extensions;
-    var schemaExtensionsProperties = schemaExtensions && schemaExtensions.properties;
 
-    for (var key in schemaExtensionsProperties) {
-      if (schemaExtensionsProperties.hasOwnProperty(key) &&
-        !_.contains(enabledExtensions, key)) {
-        delete schemaExtensionsProperties[key];
-      }
-    }
+    trimEmptyProperties(schema._extensions, enabledExtensions);
+    trimEmptyProperties(schema.menuSettings, [config.get('_menu')]);
+    trimEmptyProperties(schema.themeSettings, [config.get('_theme')]);
 
     if (schemaName === 'course') {
-      // remove unrequired globals from the course
       var globals = schema._globals.properties;
-      var extensionGlobals = globals._extensions.properties;
-      var componentGlobals = globals._components.properties;
       var enabledComponents = _.pluck(config.get('_enabledComponents'), '_component');
-
-      for (key in extensionGlobals) {
-        if (extensionGlobals.hasOwnProperty(key) &&
-          !_.contains(enabledExtensions, key)) {
-          delete extensionGlobals[key];
-        }
-      }
-
-      for (key in componentGlobals) {
-        if (componentGlobals.hasOwnProperty(key) &&
-          !_.contains(enabledComponents, key)) {
-          delete componentGlobals[key];
-        }
-      }
-
+      // remove unrequired globals from the course
+      trimEmptyProperties(globals._extensions, enabledExtensions);
+      trimEmptyProperties(globals._components, enabledComponents);
+      trimEmptyProperties(globals, enabledComponents);
       // trim off the empty globals objects
-      for (key in globals) {
-        if (globals.hasOwnProperty(key) && _.isEmpty(globals[key].properties)) {
-          delete globals[key];
-        }
-      }
+      trimEmptyProperties(globals);
     }
-
     // trim off any empty fieldsets
-    for (key in schema) {
-      if (!schema.hasOwnProperty(key)) continue;
-
-      var properties = schema[key].properties;
-
-      if (properties && _.isEmpty(properties)) {
-        delete schema[key];
-      }
-    }
+    trimEmptyProperties(schema);
 
     return schema;
   };
 
-  return Schemas;
+  function trimEmptyProperties(schemaData, enabledPlugins) {
+    var properties = schemaData && schemaData.properties;
+    for (var key in properties) {
+      if (!properties.hasOwnProperty(key)) continue;
+      if (!_.contains(enabledPlugins, key)) delete properties[key];
+    }
+  }
 
+  function trimEmptyProperties(object) {
+    for (key in object) {
+      if (!object.hasOwnProperty(key)) continue;
+      var properties = object[key].properties;
+      if (properties && _.isEmpty(properties)) delete object[key];
+    }
+  }
+
+  return Schemas;
 });

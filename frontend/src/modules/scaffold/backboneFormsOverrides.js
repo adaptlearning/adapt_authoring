5f808b3544edce293c411831342d6ec6c2af1821
Thomas Taylor <thomas.taylor@kineo.com>
2018-07-24 17:24:56 +0100

    Remove Backbone.Form.List overrides

--- frontend/src/modules/scaffold/backboneFormsOverrides.js
+++ frontend/src/modules/scaffold/backboneFormsOverrides.js
@@ -1,14 +1,12 @@
 define([
   'core/origin',
-  'backbone-forms',
-  'backbone-forms-lists',
-], function(Origin, BackboneForms, BackboneFormsLists) {
+  'backbone-forms'
+], function(Origin, BackboneForms) {
 
   var templates = Handlebars.templates;
   var fieldTemplate = templates.field;
   var templateData = Backbone.Form.Field.prototype.templateData;
   var initialize = Backbone.Form.editors.Base.prototype.initialize;
-  var listSetValue = Backbone.Form.editors.List.prototype.setValue;
   var textInitialize = Backbone.Form.editors.Text.prototype.initialize;
   var textAreaRender = Backbone.Form.editors.TextArea.prototype.render;
   var textAreaSetValue = Backbone.Form.editors.TextArea.prototype.setValue;
@@ -17,8 +15,6 @@ define([
   Backbone.Form.Fieldset.prototype.template = templates.fieldset;
   Backbone.Form.Field.prototype.template = fieldTemplate;
   Backbone.Form.NestedField.prototype.template = fieldTemplate;
-  Backbone.Form.editors.List.prototype.constructor.template = templates.list;
-  Backbone.Form.editors.List.Item.prototype.constructor.template = templates.listItem;
 
   // add reset to default handler
   Backbone.Form.Field.prototype.events = {
@@ -55,127 +51,6 @@ define([
     });
 
     initialize.call(this, options);
-  };
-
-  // process nested items
-  Backbone.Form.editors.List.Modal.prototype.itemToString = function(value) {
-    var createTitle = function(key) {
-      var context = { key: key };
-
-      return Backbone.Form.Field.prototype.createTitle.call(context);
-    };
-
-    value = value || {};
-
-    //Pretty print the object keys and values
-    var itemString = Origin.l10n.t('app.item');
-    var itemsString = Origin.l10n.t('app.items');
-    var parts = [];
-    _.each(this.nestedSchema, function(schema, key) {
-      var desc = schema.title ? schema.title : createTitle(key),
-          val = value[key],
-          pairs = '';
-
-      if (Array.isArray(val)) {
-        // print length
-        val = val.length + ' ' + (val.length === 1 ? itemString : itemsString);
-      } else if (typeof val === 'object') {
-        // print nested name/value pairs
-        for (var name in val) {
-          if (val.hasOwnProperty(name)) {
-            pairs += '<br>' + name + ' - ' + val[name];
-          }
-        }
-
-        val = pairs;
-      }
-
-      if (_.isUndefined(val) || _.isNull(val)) val = '';
-
-      // embolden key
-      parts.push('<b>' + desc + '</b>: ' + val);
-    });
-
-    return parts.join('<br />');
-  };
-
-  // fix rerendering of lists
-  // see https://github.com/powmedia/backbone-forms/pull/372
-  Backbone.Form.editors.List.prototype.setValue = function(value) {
-    this.items = [];
-
-    listSetValue.call(this, value);
-  }
-
-  Backbone.Form.editors.List.prototype.render = function() {
-    var self = this,
-        value = this.value || [],
-        $ = Backbone.$;
-
-    //Create main element
-    var $el = $($.trim(this.template({
-      addLabel: this.schema.addLabel
-    })));
-
-    //Store a reference to the list (item container)
-    this.$list = $el.is('[data-items]') ? $el : $el.find('[data-items]');
-
-    //Add existing items
-    if (value.length) {
-      _.each(value, function(itemValue) {
-        self.addItem(itemValue);
-      });
-    }
-
-    //If no existing items create an empty one, unless the editor specifies otherwise
-    else {
-      if (!this.Editor.isAsync) this.addItem();
-    }
-
-    // cache existing element
-    var domReferencedElement = this.el;
-
-    this.setElement($el);
-
-    // replace existing element
-    if (domReferencedElement) {
-      $(domReferencedElement).replaceWith(this.el);
-    }
-
-    this.$el.attr('id', this.id);
-    this.$el.attr('name', this.key);
-
-    if (this.hasFocus) this.trigger('blur', this);
-
-    return this;
-  }
-
-  // accomodate sweetalert in item removal
-  Backbone.Form.editors.List.prototype.removeItem = function(item) {
-    //Confirm delete
-    var confirmMsg = this.schema.confirmDelete;
-
-    var remove = function(isConfirmed) {
-      if (isConfirmed === false) return;
-
-      var index = _.indexOf(this.items, item);
-
-      this.items[index].remove();
-      this.items.splice(index, 1);
-
-      if (item.addEventTriggered) {
-        this.trigger('remove', this, item.editor);
-        this.trigger('change', this);
-      }
-
-      if (!this.items.length && !this.Editor.isAsync) this.addItem();
-    }.bind(this);
-
-    if (confirmMsg) {
-      window.confirm({ title: confirmMsg, type: 'warning', callback: remove });
-    } else {
-      remove();
-    }
   };
 
   // disable automatic completion on text fields if not specified

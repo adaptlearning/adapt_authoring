b71fbd51e66cacd803a2c668a9b614ab3171e40e
Tom Taylor <hello@tomtaylor.name>
2018-09-04 10:07:42 +0100

    Fix merge error

--- frontend/src/modules/scaffold/index.js
+++ frontend/src/modules/scaffold/index.js
@@ -12,7 +12,7 @@ define([
   './views/scaffoldItemsModalView',
   './views/scaffoldListView',
   './views/scaffoldTagsView'
-], function(Origin, Helpers, Schemas, BackboneForms, BackboneFormsLists, Overrides, ScaffoldAssetView, ScaffoldCodeEditorView, ScaffoldColorPickerView, ScaffoldDisplayTitleView, ScaffoldItemsModalView, ScaffoldListView, ScaffoldTagsView) {
+], function(Origin, Helpers, Schemas, BackboneForms, BackboneFormsLists, Overrides, ScaffoldAssetView, ScaffoldCodeEditorView, ScaffoldColourPickerView, ScaffoldDisplayTitleView, ScaffoldItemsModalView, ScaffoldListView, ScaffoldTagsView) {
 
   var Scaffold = {};
   var builtSchemas = {};

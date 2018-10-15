1fe0e320f95faf964a3570f8f180ecc52db27553
Thomas Berger <thomas.berger@learnchamp.com>
2018-10-15 10:47:59 +0200

    add replaceSchema helper

--- migrations/helpers/helper.js
+++ migrations/helpers/helper.js
@@ -1,10 +1,12 @@
 //add any helper methods that all migrations can use here
-var origin = require('./../../lib/application');
-var logger = require('./../../lib/logger');
+const origin = require('./../../lib/application');
+const logger = require('./../../lib/logger');
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = {
   start: function(callback) {
-    var app = origin();
+    const app = origin();
 
     //App is already running
     if(app._httpServer){
@@ -17,6 +19,16 @@ module.exports = {
     app.run({ skipVersionCheck: true, skipStartLog: true });
     app.on('serverStarted', function() {
       return callback();
+    });
+  },
+
+  replaceSchema: function(schemaName, destination, callback) {
+    fs.readJson(path.join(__dirname, '..', 'schema', schemaName + '.schema'), function(error, data) {
+      if (error) return callback(error);
+      fs.writeJson(destination, data, {spaces: 2}, function(error) {
+        if (error) return callback(error);
+        return callback(null);
+      });
     });
   }
 

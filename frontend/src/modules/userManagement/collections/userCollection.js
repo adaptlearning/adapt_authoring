5fa2d718c4a9d3107df471ad2566a630cd718b8f
Thomas Berger <thomas.berger@learnchamp.com>
2018-10-30 13:17:58 +0100

    add ability to sort users add ability to search by email

--- frontend/src/modules/userManagement/collections/userCollection.js
+++ frontend/src/modules/userManagement/collections/userCollection.js
@@ -2,13 +2,59 @@
 define(function(require) {
   var Backbone = require('backbone');
   var UserModel = require('../models/userModel');
+  var Moment = require('moment');
 
   var UserCollection = Backbone.Collection.extend({
     model: UserModel,
-    comparator: function(model) {
-      return model.get('email').toLowerCase();
+
+    sortBy: 'email',
+    direction: 1,
+    mailSearchTerm: false,
+
+    comparator: function(ma, mb) {
+      var a = ma.get(this.sortBy);
+      var b = mb.get(this.sortBy);
+      if (Array.isArray(a) && Array.isArray(b)) {
+        a = a[0];
+        b = b[0];
+      }
+      if (this.sortBy === 'lastAccess') {
+        a = new Date(a || '01.01.1900');
+        b = new Date(b || '01.01.1900');
+      }
+      if (typeof a === 'string' && typeof b === 'string') {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+      }
+      if (a > b) return this.direction;
+      else if (a < b) return this.direction*-1;
+      return 0;
     },
-    url: 'api/user'
+
+    url: 'api/user',
+
+    sortCollection: function() {
+      this.searchByMail();
+      this.sort();
+    },
+
+    resetSearchByMail: function() {
+      this.forEach(function(model) {
+        model.set('_isHidden', false);
+      });
+    },
+
+    searchByMail: function() {
+      this.resetSearchByMail();
+      if (!this.mailSearchTerm) return;
+      this.models.forEach(function(model) {
+        var mail = model.get('email').toLowerCase();
+        if (mail.indexOf(this.mailSearchTerm) < 0) {
+          model.set('_isHidden', true);
+        }
+      }, this);
+    }
+
   });
 
   return UserCollection;
--- frontend/src/modules/userManagement/views/userManagementView.js
+++ frontend/src/modules/userManagement/views/userManagementView.js
@@ -8,20 +8,28 @@ define(function(require){
   var UserView = require('../views/userView');
 
   var UserManagementView = OriginView.extend({
-    tagName: 'div',
     className: 'userManagement',
     settings: {
       autoRender: false
     },
     users: new UserCollection(),
     views: [],
+    selectedView: null,
 
     events: {
-      'click button.refresh-all': 'refreshUserViews'
+      'click button.refresh-all': 'refreshUserViews',
+      'click button[data-sort]': 'sortCollection',
+      'click .reset-filter': 'resetFilter',
+      'input .search-email': 'onSearchInput'
     },
 
     initialize: function() {
       OriginView.prototype.initialize.apply(this, arguments);
+
+      this.listenTo(this.users, 'sort', function(a,b,c) {
+        this.removeChildViews();
+        this.renderChildViews();
+      });
 
       Origin.trigger('location:title:update', { title: Origin.l10n.t('app.usermanagementtitle') });
       this.initData();
@@ -33,25 +41,36 @@ define(function(require){
     },
 
     render: function() {
-      var selectedId;
-      // remove the old views
+      this.removeChildViews();
+      OriginView.prototype.render.apply(this, arguments);
+      this.renderChildViews();
+    },
+
+    renderChildViews: function() {
+      var fragment = document.createDocumentFragment();
+      this.users.each(function(user) {
+        user.set('globalData', this.model.get('globalData'));
+        var userView = new UserView({model: user});
+        fragment.appendChild(userView.el);
+        this.views.push(userView);
+
+        if(this.selectedView && user.get('_id') === this.selectedView) {
+          userView.$el.addClass('selected').click();
+        }
+
+      }, this);
+      this.$('.users').append(fragment);
+    },
+
+    removeChildViews: function() {
       if(this.views.length) {
         for(var i = 0, count = this.views.length; i < count; i++) {
           var view = this.views[i];
-          if(view.isSelected) selectedId = view.model.get('_id');
+          if (view.isSelected) this.selectedView = view.model.get('_id');
           view.remove();
         }
         this.views = [];
       }
-
-      OriginView.prototype.render.apply(this, arguments);
-
-      this.users.each(function(user) {
-        var view = this.createUserView(user);
-        if(user.get('_id') === selectedId) {
-          view.$el.addClass('selected').click();
-        }
-      }, this);
     },
 
     postRender: function() {
@@ -59,17 +78,48 @@ define(function(require){
       this.$('.users').fadeIn(300);
     },
 
+    sortCollection: function(event) {
+      var $elm = $(event.currentTarget);
+      var sortBy = $elm.data('sort');
+      var sortAscending = $elm.hasClass('fa-sort-down');
+
+      if ($elm.hasClass('active')) {
+        sortAscending = !sortAscending;
+      }
+
+      this.$('.sort').removeClass('active fa-sort-up').addClass('fa-sort-down');
+      $elm.addClass('active');
+
+      $elm.toggleClass('fa-sort-down', sortAscending);
+      $elm.toggleClass('fa-sort-up', !sortAscending);
+
+      this.users.sortBy = sortBy;
+      this.users.direction = (sortAscending) ? 1 : -1;
+      this.users.sortCollection();
+    },
+
+    onSearchInput: function(event) {
+      var searchTerm = $(event.currentTarget).val();
+      this.users.mailSearchTerm = searchTerm;
+      this.users.sortCollection();
+    },
+
+    resetFilter: function() {
+      this.$('.sort').removeClass('active fa-sort-up').addClass('fa-sort-down');
+      this.users.sortBy = 'email';
+      this.users.direction = 1;
+      this.users.mailSearchTerm = false;
+      this.$('.search-email').val('');
+      this.$('.sort').removeClass('active fa-sort-up').addClass('fa-sort-down');
+      this.$('button[data-sort="email"]').addClass('active');
+
+      this.users.sortCollection();
+    },
+
     refreshUserViews: function(event) {
       event && event.preventDefault();
       this.users.fetch();
-    },
-
-    createUserView: function(model) {
-      model.set('globalData', this.model.get('globalData'));
-      var uv = new UserView({ model:model });
-      this.$('.users').append(uv.$el);
-      this.views.push(uv);
-      return uv;
+      this.resetFilter();
     },
 
     onDataFetched: function(models, reponse, options) {

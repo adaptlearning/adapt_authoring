d1947083afc31fd3c286b15a8273a25dd6a4f3fb
Thomas Taylor <thomas.taylor@kineo.com>
2018-11-30 14:01:25 +0000

    Fix issue with editor data load order

--- frontend/src/modules/editor/index.js
+++ frontend/src/modules/editor/index.js
@@ -2,20 +2,28 @@
 define([
   'require',
   'core/origin',
-  './global/editorDataLoader',
-  './global/editorContentEditRouter',
-  './global/editorEventHub',
-  './global/editorSidebarLinkRouter',
-  './config/index',
-  './contentObject/index',
-  './course/index',
-  './extensions/index',
-  './menuSettings/index',
-  './selectTheme/index'
+  './global/editorDataLoader'
 ], function(require, Origin, EditorData) {
+  /*
+  * Load the appropriate editor data when needed
+  */
   Origin.on({
     'origin:dataReady login:changed': EditorData.loadGlobalData,
     'router:editor editor:refreshData': EditorData.loadCourseData,
     'editor:resetData': EditorData.reset
   });
+  /**
+  * Make sure we load the below files AFTER the editor data has finished loading
+  */
+  require([
+    './global/editorContentEditRouter',
+    './global/editorEventHub',
+    './global/editorSidebarLinkRouter',
+    './config/index',
+    './contentObject/index',
+    './course/index',
+    './extensions/index',
+    './menuSettings/index',
+    './selectTheme/index'
+  ]);
 });

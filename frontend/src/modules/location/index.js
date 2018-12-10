0f9ace73b72ce59f439795e4fc893d9989f97f2f
Thomas Taylor <thomas.taylor@kineo.com>
2018-12-10 15:20:34 +0000

    Latest updates

--- frontend/src/modules/location/index.js
+++ frontend/src/modules/location/index.js
@@ -1,9 +1,9 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
   var Origin = require('core/origin');
-  var LocationTitleView = require('./views/locationTitleView');
+  var LocationView = require('./views/locationView');
 
   Origin.once('origin:dataReady', function() {
-    $('.location-title').html(new LocationTitleView().$el);
+    $('.location-title').html(new LocationView().$el);
   });
 })

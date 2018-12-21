ca5fc91b276abe60393bbf7f67e246afb2bb9580
Thomas Berger <thomas.berger@learnchamp.com>
2018-12-21 09:29:18 +0100

    update to use ffprobe instead of node-ffprobe use local path to ffmpeg and ffprobe

--- plugins/filestorage/localfs/index.js
+++ plugins/filestorage/localfs/index.js
@@ -2,17 +2,21 @@
 /**
  * Local LocalFileStorage module
  */
-var async = require('async');
-var ffmpeg = require('fluent-ffmpeg');
-var fs = require('fs-extra');
-var path = require('path');
-var probe = require('node-ffprobe');
-var util = require('util');
+const async = require('async');
+const ffmpegStatic = require('ffmpeg-static');
+const ffprobeStatic = require('ffprobe-static');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs-extra');
+const path = require('path');
+const ffprobe = require('ffprobe');
+const util = require('util');
 
-var configuration = require('../../../lib/configuration');
-var FileStorage = require('../../../lib/filestorage').FileStorage;
-var logger = require('../../../lib/logger');
-var usermanager = require('../../../lib/usermanager');
+ffmpeg.setFfmpegPath(ffmpegStatic.path);
+
+const configuration = require('../../../lib/configuration');
+const FileStorage = require('../../../lib/filestorage').FileStorage;
+const logger = require('../../../lib/logger');
+const usermanager = require('../../../lib/usermanager');
 
 function LocalFileStorage() {
   this.dataRoot = path.join(configuration.serverRoot, configuration.getConfig('dataRoot'));
@@ -442,7 +446,7 @@ LocalFileStorage.prototype.inspectFile = function (filePath, fileType, next) {
   }
 
   // Interrogate the uploaded file
-  probe(filePath, function (err, probeData) {
+  ffprobe(filePath, { path: ffprobeStatic.path }, function (err, probeData) {
     if (probeData) {
       // Store extra metadata depending on the type of file uploaded
       switch (fileType) {
@@ -454,14 +458,14 @@ LocalFileStorage.prototype.inspectFile = function (filePath, fileType, next) {
           break;
         case 'video':
           data.metadata = {
-            duration: probeData.streams[0].duration,
+            duration: parseInt(probeData.streams[0].duration),
             width: probeData.streams[0].width,
             height: probeData.streams[0].height
           }
           break;
         case 'audio':
           data.metadata = {
-            duration: probeData.streams[0].duration
+            duration: parseInt(probeData.streams[0].duration)
           }
           break;
       }

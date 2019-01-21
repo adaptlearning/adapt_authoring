9b9a6d4b16b4d97fb012eee68d76c0e15b7d415c
Thomas Berger <thomas.berger@learnchamp.com>
2019-01-21 11:16:54 +0100

    use async await

--- routes/index/index.js
+++ routes/index/index.js
@@ -6,40 +6,39 @@ const app = origin();
 const installHelper = require('../../lib/installHelpers');
 const logger = require('../../lib/logger');
 
-let versions = {};
+let _versions = {};
 
 server.set('views', __dirname);
 server.set('view engine', 'hbs');
 
-server.get('/', function (req, res, next) {
+server.get('/', async function (req, res, next) {
   const dateStamp = new Date();
   const dateStampAsString = dateStamp.toISOString().replace(/[^0-9]/g, '');
   const isProduction = configuration.getConfig('isProduction');
   const useAnalytics = configuration.getConfig('useAnalytics') || false;
   const trackingId = configuration.getConfig('trackingId') || 'UA-XXXXX-Y';
+  const versions = await getVersions();
 
-  getVersions(function(versions) {
-    res.render('index', Object.assign({
-      isProduction: isProduction,
-      dateStampAsString: dateStampAsString,
-      loading: app.polyglot.t('app.loading'),
-      productName: app.polyglot.t('app.productname'),
-      useAnalytics: useAnalytics,
-      trackingId: trackingId
-    }, versions));
-  });
+  res.render('index', Object.assign({
+    isProduction: isProduction,
+    dateStampAsString: dateStampAsString,
+    loading: app.polyglot.t('app.loading'),
+    productName: app.polyglot.t('app.productname'),
+    useAnalytics: useAnalytics,
+    trackingId: trackingId
+  }, versions));
 });
 
 function getVersions(cb) {
-  if (Object.keys(versions).length) {
-    return cb(versions);
-  }
+  return new Promise((resolve, reject) => {
+    if (Object.keys(_versions).length) return resolve(_versions);
 
-  installHelper.getInstalledVersions(function(error, data) {
-    if (error) {
-      logger.log('error', error);
-    }
-    versions = data;
-    cb(versions);
+    installHelper.getInstalledVersions((error, data) => {
+      if (error) {
+        logger.log('error', error);
+      }
+      _versions = data;
+      resolve(data);
+    });
   });
 }

0e08875c7804e2442419f33c270ab883fa6f28e1
Thomas Taylor <thomas.taylor@kineo.com>
2019-02-12 16:31:12 +0000

    Fix typo in map function
    Fixes #2223

--- frontend/src/modules/scaffold/schemas.js
+++ frontend/src/modules/scaffold/schemas.js
@@ -29,17 +29,17 @@ define([ 'core/origin', './models/schemasModel' ], function(Origin, SchemasModel
     // trim unnecessary data for menus and themes
     ['menu','theme'].forEach(function(type) {
       var current = editorData[type + 'types'].findWhere({ name: configModel.get('_' + type) });
-      if(current) trimDisabledPlugins(schema[type + 'Settings'], [current.toJSON()], type);
+      if(current) trimDisabledPlugins(schema[type + 'Settings'], [current.toJSON()]);
     });
   }
 
   // remove unrequired globals from the course
   function trimGlobals(schema) {
     var globals = schema._globals.properties;
-    trimDisabledPlugins(globals._extensions, _.values(configModel.get('_enabledExtensions')), 'targetAttribute');
+    trimDisabledPlugins(globals._extensions, _.values(configModel.get('_enabledExtensions')));
     trimDisabledPlugins(globals._components, configModel.get('_enabledComponents'), '_component');
-    trimDisabledPlugins(globals._menu, editorData.menutypes.where({ name: configModel.get('_menu') }), 'menu');
-    trimDisabledPlugins(globals._theme, editorData.themetypes.where({ name: configModel.get('_theme') }), 'theme');
+    trimDisabledPlugins(globals._menu, editorData.menutypes.where({ name: configModel.get('_menu') }));
+    trimDisabledPlugins(globals._theme, editorData.themetypes.where({ name: configModel.get('_theme') }));
     // trim off the empty globals objects
     trimEmptyProperties(globals);
   }
@@ -48,8 +48,11 @@ define([ 'core/origin', './models/schemasModel' ], function(Origin, SchemasModel
     if(!schemaData || !schemaData.properties) {
       return;
     }
+    if(!propertyToCheck) {
+      propertyToCheck = 'targetAttribute';
+    }
     var enabledPluginNames = enabledPlugins.map(function(plugin) {
-      return plugin.targetAttribute || plugin.get && plugin.get('targetAttribute');
+      return plugin[propertyToCheck] || plugin.get && plugin.get(propertyToCheck);
     });
     Object.keys(schemaData.properties).forEach(function(schemaKey) {
       if(enabledPluginNames.indexOf(schemaKey) === -1) delete schemaData.properties[schemaKey];

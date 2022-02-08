9be2dc15bf21b35364de8367c2138f376bbbcd85
Tom Taylor <hello@tomtaylor.name>
2022-02-08 18:12:53 +0000

    Make course rebuild configurable for exports

--- plugins/output/adapt/export.js
+++ plugins/output/adapt/export.js
@@ -18,8 +18,11 @@ let COURSE_DIR;
 let EXPORT_DIR;
 let TENANT_ID;
 let COURSE_ID;
+let OPTIONS = {
+  forceRebuild: true
+};
 
-function exportCourse(pCourseId, request, response, next) {
+function exportCourse(pCourseId, request, response, next, options = {}) {
   self = this;
   const currentUser = usermanager.getCurrentUser();
 
@@ -27,6 +30,7 @@ function exportCourse(pCourseId, request, response, next) {
   COURSE_ID = pCourseId;
   COURSE_DIR = path.join(FRAMEWORK_ROOT_DIR, Constants.Folders.AllCourses, TENANT_ID, COURSE_ID);
   EXPORT_DIR = path.join(configuration.tempDir, configuration.getConfig('masterTenantID'), Constants.Folders.Exports, currentUser._id);
+  Object.assign(OPTIONS, options);
 
   async.auto({
     ensureExportDir: ensureExportDir,
@@ -44,7 +48,9 @@ function ensureExportDir(exportDirEnsured) {
 }
 
 function generateLatestBuild(results, courseBuilt) {
+  if(OPTIONS.forceRebuild) {
   self.publish(COURSE_ID, Constants.Modes.Export, null, null, courseBuilt);
+  }
 }
 
 /**

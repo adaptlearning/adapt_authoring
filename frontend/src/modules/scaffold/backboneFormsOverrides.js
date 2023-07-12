1ec102c612e0bdbc79328a46ff2ea821e99dd9ef
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2023-07-12 14:57:06 -0400

    added fix to bring back paste in CK editor

--- frontend/src/modules/scaffold/backboneFormsOverrides.js
+++ frontend/src/modules/scaffold/backboneFormsOverrides.js
@@ -119,6 +119,18 @@ define([
           { name: 'others', items: [ '-' ] }
         ]
       });
+      CKEDITOR.on("instanceReady", function(event) {
+        event.editor.on("beforeCommandExec", function(event) {
+            // Show the paste dialog for the paste buttons and right-click paste
+            if (event.data.name == "paste") {
+                event.editor._.forcePasteDialog = true;
+            }
+            // Don't show the paste dialog for Ctrl+Shift+V
+            if (event.data.name == "pastetext" && event.data.commandData.from == "keystrokeHandler") {
+                event.cancel();
+            }
+        })
+      });
     }.bind(this));
 
     return this;

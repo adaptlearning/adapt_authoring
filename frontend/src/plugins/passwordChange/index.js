38dd5c03aac5e4a12a3772e26b09e324b663ca1c
Francis Tourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2023-07-16 00:55:44 -0400

    adjusted with new knowledge of notify

--- frontend/src/plugins/passwordChange/index.js
+++ frontend/src/plugins/passwordChange/index.js
@@ -1,30 +1,37 @@
 define(function (require) {
     var Origin = require('core/origin');
     var UserInfoModel = require('./models/userInfoModel');
-    console.log(Origin.Notify);
 
-    Origin.once('origin:dataReady login:changed', function(){
+    Origin.once('origin:dataReady login:changed location:change', function () {
         var user = new UserInfoModel();
         user.fetch({
             success: function () {
-                var text = $('html').attr('lang') === 'en' ? 'Please <a href="#user/profile">change your password</a>! This message will be dismissed once it is changed.' : 'Veuillez <a href="#user/profile">changer votre mot de passe</a>! Ce message cessera lorsque vous aurez changé votre mot de passe';
-                if(user.attributes.lastPasswordChange){
-                    var policyChange = new Date('2023-06-26T01:12:06.510Z')
-                    var changeDate = new Date(user.attributes.lastPasswordChange)
-                    if((changeDate > policyChange)){
-                       Origin.Notify.alert({
-                        type: 'warning',
-                        text: text
-                      });
-                    }
-                } else {
+                function openPopup(){
                     Origin.Notify.alert({
                         type: 'warning',
-                        text: text
-                      });
+                        text: Origin.l10n.t('app.passwordchangemessage'),
+                        showConfirmButton: true,
+                        confirmButtonText: 'Go to Profile',
+                        closeOnConfirm: true,
+                        showCancelButton: false,
+                        callback: function (isConfirm) {
+                            if (isConfirm) {
+                                Origin.router.navigateTo('user/profile');
+                            }
+                        }
+                    });
+                }
+                if (user.attributes.lastPasswordChange) {
+                    console.log(user.attributes.lastPasswordChange);
+                    var policyChange = new Date('2023-06-26T01:12:06.510Z')
+                    var changeDate = new Date(user.attributes.lastPasswordChange)
+                    if ((changeDate < policyChange)) {
+                       openPopup();
+                    }
+                } else {
+                    openPopup();
                 }
             }
-        });
+        })
     })
 })
-  
